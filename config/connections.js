90e69c26ab60e0cc3c3c3148200dfff2e165797f
bsoufflet <benjamin@fruitionsciences.com>
2015-06-13 01:46:10 +0200

    setup db connection

--- config/connections.js
+++ config/connections.js
@@ -28,9 +28,9 @@ module.exports.connections = {
   * Installed by default.                                                    *
   *                                                                          *
   ***************************************************************************/
-  localDiskDb: {
-    adapter: 'sails-disk'
-  },
+  //localDiskDb: {
+  //  adapter: 'sails-disk'
+  //},
 
   /***************************************************************************
   *                                                                          *
@@ -40,12 +40,13 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  someMysqlServer: {
+  grottoceMysql: {
+    migrate: 'safe',
     adapter: 'sails-mysql',
     host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
     user: 'YOUR_MYSQL_USER',
     password: 'YOUR_MYSQL_PASSWORD',
-    database: 'YOUR_MYSQL_DB'
+    database: 'grottoce'
   },
 
   /***************************************************************************
@@ -56,14 +57,14 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
-    adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
+  //someMongodbServer: {
+  //  adapter: 'sails-mongo',
+  //  host: 'localhost',
+  //  port: 27017,
     // user: 'username',
     // password: 'password',
     // database: 'your_mongo_db_name_here'
-  },
+  //},
 
   /***************************************************************************
   *                                                                          *
@@ -74,13 +75,13 @@ module.exports.connections = {
   *                                                                          *
   *                                                                          *
   ***************************************************************************/
-  somePostgresqlServer: {
-    adapter: 'sails-postgresql',
-    host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_POSTGRES_USER',
-    password: 'YOUR_POSTGRES_PASSWORD',
-    database: 'YOUR_POSTGRES_DB'
-  }
+  //somePostgresqlServer: {
+  //  adapter: 'sails-postgresql',
+  //  host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
+  //  user: 'YOUR_POSTGRES_USER',
+  //  password: 'YOUR_POSTGRES_PASSWORD',
+  //  database: 'YOUR_POSTGRES_DB'
+  //}
 
 
   /***************************************************************************
