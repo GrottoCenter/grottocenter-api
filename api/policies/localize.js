847cb3e8a37aa7137319965d7f38bca01770b22a
Didine74 <a.panier@free.fr>
2016-09-29 21:18:49 +0200

    fix error when no language is passed in parameter (English by default)

--- api/policies/localize.js
+++ api/policies/localize.js
@@ -3,6 +3,9 @@ module.exports = function(req, res, next) {
 	if (req.param('lang') != undefined) {
 		req.session.languagePreference = req.param('lang');
 	}
+	if (req.session.languagePreference == undefined) {
+		req.session.languagePreference = "EN";
+	}
 	if (req.session.languagePreference != "") {
 		req.setLocale(req.session.languagePreference);
 	}
