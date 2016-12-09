e05280fdf94a3fcfc6acb543b6a02727f0828631
fabricez <fab.augier@gmail.com>
2016-12-09 11:22:01 +0100

    deactivate eslint linebreak error + remove react-google-maps + add eslint config

---
+++ config/eslint.js
@@ -0,0 +1,12 @@
+module.exports.eslint = {
+    active: true,
+    usePolling: false,
+    formatter: 'stylish',
+    dirs: [
+    "api",
+    "config"
+  ],
+    ignored: [
+    "api/policies"
+  ]
+};
