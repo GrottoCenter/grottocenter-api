544fca53ccd486161097bd90168bfecc219bcc0f
kurdty <kurdty@free.fr>
2017-01-05 22:53:41 +0100

    Review the FSE link (link now depend on selected language)

--- assets/js/pages/homepage/Header.jsx
+++ assets/js/pages/homepage/Header.jsx
@@ -1,40 +1,58 @@
-import React from 'react';
-import SearchBar from './../../components/searchBar/Bar';
+import React, {Component} from 'react';
 import Autocomplete from './../../components/searchBar/Autocomplete';
 import I18n from 'react-ghost-i18n';
 import muiThemeable from 'material-ui/styles/muiThemeable';
 
-const Header = (props) => (
-  <header className="header" style={{color: props.muiTheme.palette.primaryTextColor, fontFamily: props.muiTheme.fontFamily}}>
-    <div className="container">
-      <div className="row brand bgwhite">
-        <div className="twelve columns">
-          <a href="" className="logo">
-            <img src="/images/logo.svg" alt="logo-wikicaves"/>
-          </a>
-          <h1 className="sitename">Grottocenter</h1>
-          <span className="slogan">
-              <I18n>The Wiki database made by cavers for cavers.</I18n>
-          </span>
-        </div>
-      </div>
+let fseLinks = {
+  'fr': 'http://eurospeleo.eu/fr/'
+};
 
-      <div className="row bgwhite">
-        <div className="twelve columns">
-          <span className="gc-fse-info">
-            <img className="logofse" src="/images/FSE.svg" alt="logo-fse"/>
-            <I18n>Grottocenter is supported by the FSE</I18n>
-          </span>
-        </div>
-      </div>
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      locale: locale // eslint-disable-line no-undef
+    };
+  }
 
-      <div className="row searchbar">
-        <div className="twelve columns">
-          <Autocomplete/>
+  render() {
+    let fseLink = (fseLinks[this.state.locale] !== undefined) ? fseLinks[this.state.locale] : 'http://eurospeleo.eu/en/';
+
+    return (
+      <header className="header" style={{color: this.props.muiTheme.palette.primaryTextColor, fontFamily: this.props.muiTheme.fontFamily}}>
+        <div className="container">
+          <div className="row brand bgwhite">
+            <div className="twelve columns">
+              <a href="" className="logo">
+                <img src="/images/logo.svg" alt="logo-wikicaves"/>
+              </a>
+              <h1 className="sitename">Grottocenter</h1>
+              <span className="slogan">
+                  <I18n>The Wiki database made by cavers for cavers.</I18n>
+              </span>
+            </div>
+          </div>
+
+          <div className="row bgwhite">
+            <div className="twelve columns">
+              <span className="gc-fse-info">
+                <a href={fseLink} alt="Link to FSE">
+                  <img className="logofse" src="/images/FSE.svg" alt="logo-fse"/>
+                </a>
+                <I18n>Grottocenter is supported by the FSE</I18n>
+              </span>
+            </div>
+          </div>
+
+          <div className="row searchbar">
+            <div className="twelve columns">
+              <Autocomplete/>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </header>
-);
+      </header>
+    );
+  }
+}
 
 export default muiThemeable()(Header);
