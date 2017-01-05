f6e0429ef072c13466e7836cbc87d9ea9e2071c1
kurdty <kurdty@free.fr>
2017-01-05 23:59:02 +0100

    Review the RSS and Blogger links (linkd now depend on selected language)

--- assets/js/pages/homepage/Footer.jsx
+++ assets/js/pages/homepage/Footer.jsx
@@ -12,15 +12,27 @@ let licenceLinks = {
   'it': 'https://creativecommons.org/licenses/by-sa/3.0/deed.it'
 };
 
-let wikiBatsLinks = {
-  'fr': 'https://wiki.grottocenter.org/wiki/GrottoCenter:Fr/bats',
-  'es': 'https://wiki.grottocenter.org/wiki/GrottoCenter:Es/bats',
-  'ca': 'https://wiki.grottocenter.org/wiki/GrottoCenter:Ca/bats',
-  'de': 'https://wiki.grottocenter.org/wiki/GrottoCenter:De/bats',
-  'bg': 'https://wiki.grottocenter.org/wiki/GrottoCenter:Bg/bats',
-  'nl': 'https://wiki.grottocenter.org/wiki/GrottoCenter:Nl/bats',
-  'it': 'https://wiki.grottocenter.org/wiki/GrottoCenter:It/bats'
+let wikiBatsLinks = {};
+localesList.map(function(el) { // eslint-disable-line no-undef
+  let language = el.value;
+  language = language.substring(0, 1).toUpperCase() + language.substring(1, language.length);
+  wikiBatsLinks[el.value] = 'https://wiki.grottocenter.org/wiki/GrottoCenter:' + language + '/bats';
+}, this);
+
+let bloggerLinks = {
+  'fr': 'http://blog-fr.grottocenter.org/'
 };
+
+let bloggerIcons = {
+  'fr': 'blogger-Fr.svg'
+};
+
+let rssLinks = {};
+localesList.map(function(el) { // eslint-disable-line no-undef
+  let language = el.value;
+  language = language.substring(0, 1).toUpperCase() + language.substring(1, language.length);
+  rssLinks[el.value] = 'http://www.grottocenter.org/html/rss_' + language + '.xml';
+}, this);
 
 class Footer extends Component {
   constructor(props) {
@@ -33,6 +45,9 @@ class Footer extends Component {
   render() {
     let computedLink = (licenceLinks[this.state.locale] !== undefined) ? licenceLinks[this.state.locale] : 'https://creativecommons.org/licenses/by-sa/3.0/';
     let wikiBatsLink = (wikiBatsLinks[this.state.locale] !== undefined) ? wikiBatsLinks[this.state.locale] : 'https://wiki.grottocenter.org/wiki/GrottoCenter:Es/bats';
+    let bloggerLink = (bloggerLinks[this.state.locale] !== undefined) ? bloggerLinks[this.state.locale] : 'http://blog-en.grottocenter.org/';
+    let bloggerIcon = (bloggerIcons[this.state.locale] !== undefined) ? bloggerIcons[this.state.locale] : 'blogger-En.svg';
+    let rssLink = (rssLinks[this.state.locale] !== undefined) ? rssLinks[this.state.locale] : 'http://www.grottocenter.org/html/rss_En.xml';
 
     return (
       <footer style={{backgroundColor: this.props.muiTheme.palette.primary1Color, color: this.props.muiTheme.palette.textIconColor}}>
@@ -46,14 +61,22 @@ class Footer extends Component {
               <a href="http://www.wikicaves.org/" target="_blank"><I18n>Wikicaves association</I18n></a>
             </div>
             <div className="three columns social">
-              <a href="https://www.facebook.com/GrottoCenter" target="_blank"><img src="/images/facebook.svg" alt=""/></a>
-              <a href="http://www.grottocenter.org/html/rss_Es.xml" target="_blank"><img src="/images/rss.png" alt=""/></a>
-              <a href="http://blog-fr.grottocenter.org/" target="_blank"><img src="/images/blog.png" alt=""/></a>
-              <a href="https://twitter.com/grottocenter" target="_blank"><img src="/images/twitter.png" alt=""/></a>
+              <a href="https://www.facebook.com/GrottoCenter" target="_blank">
+                <img src="/images/facebook.svg" alt="Follow us on Facebook"/>
+              </a>
+              <a href={rssLink} target="_blank">
+                <img src="/images/rss.png" alt="RSS feed"/>
+              </a>
+              <a href={bloggerLink} target="_blank">
+                <img src={'/images/' + bloggerIcon} alt="Grottocenter blog"/>
+              </a>
+              <a href="https://twitter.com/grottocenter" target="_blank">
+                <img src="/images/twitter.svg" alt="Follow us on Twitter"/>
+              </a>
             </div>
             <div className="three columns infos">
               <a href="https://github.com/GrottoCenter" target="_blank">
-                <img src="/images/github.png" alt=""/>
+                <img src="/images/github.png" alt="Grottocenter3 on GitHub"/>
               </a>
               <a href="modal-mail.html" data-target="#mail" data-toggle="modal" role="dialog" aria-labelledby="mail" aria-hidden="true">
                 <img src="/images/icon/ic_contact_mail_black_48px.png" alt=""/>
