0be96df217de17e4604ba9cf91111ff9998a234e
fabricez <fab.augier@gmail.com>
2017-01-06 18:46:23 +0100

    Add gc-icon svg font for specific caving symbols
    Test the gc-icon svg font on WhatIsIt component

--- assets/js/pages/homepage/WhatIsIt.jsx
+++ assets/js/pages/homepage/WhatIsIt.jsx
@@ -1,43 +1,36 @@
-import React from 'react';
-import I18n from 'react-ghost-i18n';
+import React from 'react'
+import I18n from 'react-ghost-i18n'
 
 const WhatIsIt = () => (
-  <div>
     <div role="section" className="">
       <h3  style={{'text-align': 'center', 'padding-bottom': '50px'}}>
-        <I18n>At the beginning of 2017, Grottocenter represents:</I18n>
+        <I18n>Grottocenter numbers</I18n>
+          <span className="icon icon-gc-bat" style={{'color':'black','vertical-align': 'super'}}></span><br/>
       </h3>
       <div className="container">
         <div className="row">
-          <div style={{'text-align': 'center', 'padding-top': '20px'}} className="three columns">
-            <div className="animData">16</div>
-            <div style={{'padding-top': '20px'}}>
+          <div className="six columns">
+            <span className="icon icon-gc-entries" style={{'color':'#885B39','font-size': '6em'}}></span><br/>
+            <span style={{'color':'#885B39','font-size': '3em'}}>39175 </span>
+            <I18n>caves are freely accessible from the following page (</I18n>50 902 <I18n>by logging on</I18n> <a href='http://www.grottocenter.org'>Grotto v2</a>)
+            <br/>
+            <br/>
+            <span className="icon icon-gc-speleo" style={{'color':'#885B39','font-size': '4em'}}></span><br/>
+            <span style={{'color':'#885B39','font-size': '3em'}}>2724 </span>
+            <I18n>cavers take part, day after day, in improving and expanding the database</I18n>
+          </div>
+          <div className="six columns">
+            <span className="icon icon-gc-club" style={{'color':'#885B39','font-size': '4em'}}></span><br/>
+            <span style={{'color':'#885B39','font-size': '3em'}}>16 </span>
               <I18n>organizations who take part in the project by  funding, providing data,communicating on the interest and benefits of  cavers to share data.</I18n>
-            </div>
-          </div>
-          <div style={{'text-align': 'center', 'padding-top': '20px'}} className="three columns">
-            <div className="animData">654</div>
-            <div style={{'padding-top': '20px'}}>
+            <br/>
+            <br/>
+            <span className="icon icon-gc-expe" style={{'color':'#885B39','font-size': '4em'}}></span><br/>
+            <span style={{'color':'#885B39','font-size': '3em'}}>654 </span>
               <I18n>organizations are registered on the site</I18n>
-            </div>
-          </div>
-          <div style={{'text-align': 'center', 'padding-top': '20px'}} className="three columns">
-            <div className="animData">2724</div>
-            <div style={{'padding-top': '20px'}}>
-              <I18n>cavers take part, day after day, in improving and expanding the database</I18n>
-            </div>
-          </div>
-          <div style={{'text-align': 'center', 'padding-top': '20px'}} className="three columns">
-            <div className="animData">39175</div>
-            <div style={{'padding-top': '20px'}}>
-              <I18n>caves are freely accessible from the following page (</I18n>50 902 <I18n>by logging on</I18n> <a href='http://www.grottocenter.org'>Grotto v2</a>)
-
-            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
-
-export default WhatIsIt;
+)
+export default WhatIsIt
