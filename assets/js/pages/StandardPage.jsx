b0809bc7222b2aa20abb106f6a5e56a03ae6d2b0
kurdty <kurdty@free.fr>
2017-03-01 08:49:25 +0100

    refactor(react): turn react components to pure functions

--- assets/js/pages/StandardPage.jsx
+++ assets/js/pages/StandardPage.jsx
@@ -5,23 +5,17 @@ import GrottoAppBar from '../components/homepage/GrottoAppBar';
 import Header from '../components/homepage/Header';
 import Footer from '../components/homepage/Footer';
 
-export default class StandardPage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <GrottoAppBar/>
-        <Header/>
-        {this.props.children}
-        <Footer/>
-      </div>
-    );
-  }
-}
+const StandardPage = (props) => (
+  <div>
+    <GrottoAppBar/>
+    <Header/>
+    {props.children}
+    <Footer/>
+  </div>
+);
 
 StandardPage.propTypes = {
   children: PropTypes.node.isRequired
 };
+
+export default StandardPage;
