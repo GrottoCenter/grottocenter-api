3fa63c54eadfebc3f1470192b3a80ba85603c3bb
Kurdty <kurdty@free.fr>
2017-12-09 22:55:11 +0100

    feat(api): adapt texts on API page

--- assets/js/components/Api.jsx
+++ assets/js/components/Api.jsx
@@ -17,16 +17,16 @@ const Api = (props) => (
             <h3 style={{color: props.muiTheme.palette.accent1Color}}>
               <I18n>Grottocenter API</I18n>
             </h3>
-            <p><I18n>Your website needs worldwide speleology data? Trust in Grottocenter to manage them data for you!</I18n></p>
-            <p><I18n>You can find here blablabla</I18n></p>
-            <p><I18n></I18n></p>
+            <p><I18n>You need to manipulate worldwide speleology data on your website? Trust Grottocenter to manage it for you!</I18n></p>
+            <p><I18n>We offer you a set of <GCLink href='http://www.comprendre-internet.com/Qu-est-ce-qu-une-API-REST-ou-RESTful.html'>Rest API endpoints</GCLink> that you can easily insert in your pages to access this data.</I18n></p>
+            <p></p>
             <h5><I18n>Available versions:</I18n></h5>
             <p>
               <CheckIcon style={{color: props.muiTheme.palette.accent1Color, position: 'relative', top: '6px', marginRight: '10px'}} />
               <GCLink style={{textDecoration: 'none', fontWeight: '600', color: props.muiTheme.palette.accent1Color}} internal={true} href={swaggerLinkV1}><I18n>Version 1</I18n></GCLink>
             </p>
             <p>
-              Not familiar with Swagger? Need support ? Or explain us a specific need? Contact us here: LINK TO SLACK!
+              Not familiar with Swagger? Need support? <GCLink href='https://grottocenter.slack.com/messages/C858CHARY/'>Contact us!</GCLink>
             </p>
           </div>
         </div>
