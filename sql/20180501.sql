8e3d555afb64b81a7b82fdedd073c15112575671
Urien <fredericurien@gmail.com>
2018-01-05 10:49:51 +0100

    Add Entries with content

---
+++ sql/20180501.sql
@@ -0,0 +1,30 @@
+
+-- update existing data
+update t_entry set Is_of_interest=1 where id=738;
+update t_entry set Is_of_interest=1 where id=566;
+update t_entry set Is_of_interest=1 where id=2;
+update t_entry set Is_of_interest=1 where id=660;
+update t_entry set Is_of_interest=1 where id=667;
+update t_entry set Is_of_interest=1 where id=37079;
+update t_entry set Is_of_interest=1 where id=766;
+update t_entry set Is_of_interest=1 where id=610;
+update t_entry set Is_of_interest=1 where id=588;
+update t_entry set Is_of_interest=1 where id=37077;
+update t_entry set Is_of_interest=1 where id=769;
+update t_entry set Is_of_interest=1 where id=782;
+update t_entry set Is_of_interest=1 where id=780;
+update t_entry set Is_of_interest=1 where id=37075;
+update t_entry set Is_of_interest=1 where id=571;
+update t_entry set Is_of_interest=1 where id=567;
+update t_entry set Is_of_interest=1 where id=560;
+update t_entry set Is_of_interest=1 where id=37068;
+update t_entry set Is_of_interest=1 where id=557;
+update t_entry set Is_of_interest=1 where id=551;
+update t_entry set Is_of_interest=1 where id=540;
+update t_entry set Is_of_interest=1 where id=505;
+update t_entry set Is_of_interest=1 where id=500;
+update t_entry set Is_of_interest=1 where id=37066;
+update t_entry set Is_of_interest=1 where id=810;
+update t_entry set Is_of_interest=1 where id=461;
+update t_entry set Is_of_interest=1 where id=37088;
+update t_entry set Is_of_interest=1 where id=25085;
