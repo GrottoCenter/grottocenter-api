ebb0d756b6e9c1f8c756d986d9dc44a519f8d887
Kurdty <kurdty@free.fr>
2018-03-12 15:36:44 +0100

    feat(map): rename quicksearch component and rework a little the redux process

--- assets/js/react/containers/QuicksearchContainer.jsx
+++ assets/js/react/containers/QuicksearchContainer.jsx
@@ -1,22 +1,21 @@
 import {connect} from 'react-redux';
-import {fetchQuicksearchResult, resetQuicksearch, showMarker} from './../actions/Quicksearch';
-import Autocomplete from '../components/common/Autocomplete';
-import BackgroundMap from '../components/appli/Map';
+import {fetchQuicksearchResult, resetQuicksearch, setCurrentEntry} from './../actions/Quicksearch';
+import Searchbar from '../components/common/Searchbar';
 
 const mapDispatchToProps = (dispatch, ownProps) => { // eslint-disable-line no-unused-vars
   return {
     reset: () => dispatch(resetQuicksearch()),
     search: (criteria) => dispatch(fetchQuicksearchResult(criteria)),
-    showMarker: (entry) => dispatch(showMarker(entry))
+    setCurrentEntry: (entry) => dispatch(setCurrentEntry(entry))
   };
 }
 
 const mapStateToProps = (state, ownProps) => { // eslint-disable-line no-unused-vars
   return {
     results: state.quicksearch.results,
-    marker: state.quicksearch.entry,
+    selectedEntry: state.quicksearch.entry,
     className: ownProps.className
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Autocomplete, BackgroundMap);
+export default connect(mapStateToProps, mapDispatchToProps)(Searchbar);
