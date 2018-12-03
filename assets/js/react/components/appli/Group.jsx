556eb4f7560c59a3af33a6f656f776e259f7735e
Clément Roig <clm.roig@gmail.com>
2018-12-03 15:39:45 +0100

    feat(#13): Displaying more info about group in client

--- assets/js/react/components/appli/Group.jsx
+++ assets/js/react/components/appli/Group.jsx
@@ -1,28 +1,93 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import styled from 'styled-components';
+import EmailIcon from '@material-ui/icons/Email';
 import Translate from '../common/Translate';
+
+const GroupIcon = styled.img`
+  display: inline-block;
+  margin-right: 10px;
+  vertical-align: baseline;
+  width: 3rem;
+`;
+
+const GClogo = styled.img`
+  display: inline-block;
+  vertical-align: baseline;
+  height: 3rem;
+`;
+
+const GroupName = styled.h1`
+  display: inline-block;
+`;
+
+const EmailIconStyled = withStyles({
+  root: {
+    verticalAlign: 'bottom',
+  },
+})(EmailIcon);
 
 const Group = (props) => {
   const { group } = props;
-  return (
+
+  let completeAddress = `${group.Country} - ${group.Region} - ${group.County}`;
+  if (group.City) completeAddress += ` - ${group.City}`;
+  else if (group.Village) completeAddress += ` - ${group.Village}`;
+  if (group.PostalCode) completeAddress += ` - ${group.PostalCode}`;
+  if (group.Address) completeAddress += ` - ${group.Address}`;
+
+  const result = (
     <div>
-          <h1>{group.Name}</h1>
-          <p>{group.Country}{' - '}
-            {group.Region}{' - '} 
-            {group.County}
-            {group.City ? ' - ' + group.City : (group.Village ? ' - ' + group.Village : '')}
-            {group.PostalCode ? ' - ' + group.PostalCode : ''}
-          </p>
-          <p><b><Translate>Contact</Translate></b>: {group.Contact}</p>
-          <p>
-            {group.Custom_message}
-          </p>
+      <GroupIcon src="../../../../images/club.svg" alt="Club icon" />
+      <GroupName>
+        {group.Name}
+      </GroupName>
+      <p>
+        {group.Year_birth ? (
+          <i>
+            <Translate>Since</Translate>
+            {' '}
+            {group.Year_birth}
+          </i>
+        ) : ''}
+
+        {group.Is_official_partner ? (
+          <span>
+            {group.Year_birth ? ' - ' : ''}
+            <Translate>Official partner</Translate>
+            {' '}
+            <GClogo src="../../../../images/logoGC.png" alt="GC logo" />
+          </span>
+        ) : ''}
+      </p>
+
+      <div>
+        {completeAddress}
+      </div>
+
+      {group.Contact ? (
+        <div>
+          <EmailIconStyled />
+          <b>
+            <Translate>Contact</Translate>
+          </b>
+        :
+          {' '}
+          {group.Contact}
+        </div>
+      ) : ''}
+
+      <div>
+        {group.Custom_message}
+      </div>
     </div>
   );
+  return result;
 };
 
 Group.propTypes = {
-    group: PropTypes.object.isRequired,
+  group: PropTypes.object.isRequired,
 };
 
 export default Group;
--- assets/js/react/containers/GroupContainer.jsx
+++ assets/js/react/containers/GroupContainer.jsx
@@ -1,49 +1,45 @@
 import { connect } from 'react-redux';
 import { loadGroup } from '../actions/Group';
-import Group from './../components/appli/Group';
+import Group from '../components/appli/Group';
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetch: dispatch(loadGroup())
-  };
-}
+const mapDispatchToProps = dispatch => ({
+  fetch: dispatch(loadGroup()),
+});
 
-const mapStateToProps = (state) => {
-  return {
-    // group: state.group
+const mapStateToProps = state => ({
+  // group: state.group
 
-    // TODO : currently working with fake data, use real one (see above)
-    group: {
-      "Id": "3",
-      "Locked": "NO",
-      "{{3}}')": null,
-      "Id_author": "16",
-      "Id_reviewer": "50",
-      "Id_locker": "50",
-      "Name": "CLPA",
-      "Country": "FR",
-      "Village": null,
-      "County": "Hérault",
-      "Region": "Occitanie",
-      "City": "Montpellier",
-      "Postal_code": "34000",
-      "Address": "9, rue de la Poésie",
-      "Contact": "clpa@wanadoo.fr",
-      "Year_birth": "1967",
-      "Date_inscription": "2008-07-29 12:00:48",
-      "Date_reviewed": "2016-03-26 01:30:58",
-      "Date_locked": "2016-03-26 01:30:42",
-      "Id_president": null,
-      "Id_vice_president": "314",
-      "Id_treasurer": "519",
-      "Id_secretary": null,
-      "Latitude": "43.61734339229920000000",
-      "Longitude": "3.88173580169677730000",
-      "Custom_message": "http://speleoclpa.free.fr",
-      "Picture_file_name": "clpa-3.jpg",
-      "Is_official_partner": null
-    }
-  };
-};
+  // TODO : currently working with fake data, use real one (see above)
+  group: {
+    Id: '3',
+    Locked: 'NO',
+    "{{3}}')": null,
+    Id_author: '16',
+    Id_reviewer: '50',
+    Id_locker: '50',
+    Name: 'CLPA',
+    Country: 'FR',
+    Village: null,
+    County: 'Hérault',
+    Region: 'Occitanie',
+    City: 'Montpellier',
+    Postal_code: '34000',
+    Address: '9, rue de la Poésie',
+    Contact: 'clpa@wanadoo.fr',
+    Year_birth: '1967',
+    Date_inscription: '2008-07-29 12:00:48',
+    Date_reviewed: '2016-03-26 01:30:58',
+    Date_locked: '2016-03-26 01:30:42',
+    Id_president: null,
+    Id_vice_president: '314',
+    Id_treasurer: '519',
+    Id_secretary: null,
+    Latitude: '43.61734339229920000000',
+    Longitude: '3.88173580169677730000',
+    Custom_message: 'http://speleoclpa.free.fr',
+    Picture_file_name: 'clpa-3.jpg',
+    Is_official_partner: 1,
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Group);
