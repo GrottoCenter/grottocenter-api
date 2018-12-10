939e0a9a1ce9cfdcb921a0891ac684b6b33f3c90
Clément Roig <clm.roig@gmail.com>
2018-12-10 17:32:13 +0100

    feat(#124): Update index content everytime  a change is made

--- api/services/ElasticsearchService.js
+++ api/services/ElasticsearchService.js
@@ -1,12 +1,16 @@
 'use strict';
 
+const client = require('../../config/elasticsearch');
+
 const resourcesToUpdate = [
   'grotto', 'caves', 'entries'
 ];
 
 module.exports = {
-  /*
-    Update the Elasticsearch index according to the request.
+  /**
+  * Update the Elasticsearch index according to the request.
+  * @param {*} found resource to be updated 
+  * @param {*} req req object of the client
   */
   updateIndex: function (found, req) {
     const { url, verb } = req;
@@ -32,18 +36,36 @@ module.exports = {
     const resourceName = urlPieces[resourceIndex];
     // Update index resources appropriately
     if(resourcesToUpdate.includes(resourceName)){
+
       if(verb === 'PUT') {
-        // TODO: update resource in index
-
+        // Asynchronous operation, no callback
+        client.update({
+          index: resourceName +'-index',
+          type: resourceName,
+          id: found.id,
+          body: found
+        });
       }
+
       if(verb === 'POST') {
-        // TODO: add resource to index
+        // Asynchronous operation, no callback
+        client.create({
+          index: resourceName +'-index',
+          type: resourceName,
+          id: found.id,
+          body: found
+        });
+      }
 
-      }
       if(verb === 'DELETE') {
-        // TODO: delete resource from index
-        
+        // Asynchronous operation, no callback
+        client.delete({
+          index: resourceName +'-index',
+          type: resourceName,
+          id: found.id
+        });        
       }
+      
     }
   },
 };
