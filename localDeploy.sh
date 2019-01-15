e87a307b0ff5cc2ae3c47e6603a4905e118dfe85
Clément Roig <clm.roig@gmail.com>
2019-01-15 09:21:17 +0100

    feat(#54): schedule logstash + clean localDeploy
    Remove grotto server container
    Better logs messages
    Logstash scheduled

--- localDeploy.sh
+++ localDeploy.sh
@@ -5,7 +5,6 @@
 #
 # This script will create and run 2 Docker containers on your local computer :
 # - {MYSQL_TAGNAME} mysql container with initial data loaded (all scripts of "sql" folder executed)
-# - {GC_TAGNAME} nodejs container with current grottocenter app loaded in production and connected to {MYSQL_TAGNAME}
 # - {ES_TAGNAME} elasticsearch container that is used for all research on the app
 # - {LS_TAGNAME} logstash container that pipe data from {MYSQL_TAGNAME} container to {ES_TAGNAME} container.
 #   The config file is the logstash.conf and populates the elasticsearch container.
@@ -16,15 +15,18 @@
 #
 
 ###################################################### LOCAL VARIABLES #########################################################
+# MySQL Database
 MYSQL_TAGNAME="mysqlgrotto"
-GC_TAGNAME="localgrotto"
 MYSQL_LOCAL_PORT=33060
-GC_LOCAL_PORT=8081
 DOCKER_MYSQL_USER="sailsuser"
 DOCKER_MYSQL_PASSWORD="grottocepassword"
 DOCKER_MYSQL_DATABASE="grottoce"
+
+# Elasticsearch
 ES_TAGNAME="elasticsearchgrotto"
 ES_LOCAL_PORT=9200
+
+# Logstash
 LS_TAGNAME="logstashgrotto"
 
 ##################################################### FUNCTIONS #########################################################
@@ -52,16 +54,18 @@ function waitContainer {
 
 ##################################################### MAIN FUNCTION #########################################################
 
-echo "### BUILDING app locally using prod tasks ###"
-NODE_ENV=production grunt prod
-
 ### RUNNING DOCKER IMAGES INSTANCES ###
 
 # Delete the MySQL container if one was running then launch a MySQL container with data loaded.
 # The health of this container is defined to healthy when we can send a ping to mysqladmin.
 # The database is populated with files in the sql folder.
+isSQLContainerExisting="$(docker ps --all --quiet --filter=name="$MYSQL_TAGNAME")"
+if [ -n "$isSQLContainerExisting" ]; then
+  echo "### DELETING old MySQL Container ###"
+  docker stop $MYSQL_TAGNAME && docker rm -f $MYSQL_TAGNAME
+fi
+
 echo "### LAUNCHING MySQL Container ###"
-docker rm -f ${MYSQL_TAGNAME}
 docker run -d \
 --name ${MYSQL_TAGNAME} \
 --health-cmd='mysqladmin ping --silent' \
@@ -80,35 +84,36 @@ echo "### MySQL is RUNNING ###"
 # The database is entirely populated when we can reach a request to mysql from outside the container
 echo "### POPULATING Database ###"
 #TODO: Handle the message when database is populated
-until curl -X GET "localhost:${MYSQL_LOCAL_PORT}" --silent
+until curl -X GET "localhost:${MYSQL_LOCAL_PORT}" --silent > /dev/null
 do
     # Print a "." and wait for 5 seconds before check again
     printf .
     sleep 5
 done
-
+echo ""
 echo "### Database POPULATED ###"
 
-echo "### BUILD the grottocenter docker image ###"
-# Unlimited swap memory during build
-docker build --memory-swap -1 -t ${GC_TAGNAME} .  || { echo '######## Build failed - Exiting now #########' ; exit 1; }
-
-#TODO: Use an ES container in production
 # Delete the Elasticsearch container if already running and then create an Elasticsearch container for development with only one node.
-echo "### BUILD the Elastic search container in development mode ###"
-docker rm -f ${ES_TAGNAME}
+isESContainerExisting="$(docker ps --all --quiet --filter=name="$ES_TAGNAME")"
+if [ -n "$isESContainerExisting" ]; then
+  echo "### DELETING old Elasticsearch Container ###"
+  docker stop $ES_TAGNAME && docker rm -f $ES_TAGNAME
+fi
+
+echo "### BUILD the Elasticsearch container in development mode ###"
 docker run -d \
     --name ${ES_TAGNAME} \
     -p ${ES_LOCAL_PORT}:9200 \
     -p 9300:9300 \
     -e "discovery.type=single-node" \
+    --cpus=0.5 \
     elasticsearch:6.5.0
 echo "### Elasticsearch available on port ${ES_LOCAL_PORT} ###"
 
 # Dowload and extract a plugin to be used by Logstash to load data from MySQL
 echo "### Download and Build the JDBC plugin for Logstash ###"
 wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.47.tar.gz -O ./mysql-connector.tar.gz
-tar -xvzf mysql-connector.tar.gz
+tar -xzf mysql-connector.tar.gz
 echo "### JDBC plugin downloaded and built ###"
 
 # Delete the Logstash container if already running and then create a Logstash container with the logstash.conf as configuration file.
@@ -116,8 +121,13 @@ echo "### JDBC plugin downloaded and built ###"
 # to access the localhost:9600
 # We set the elasticsearch url to be the same than the tagname of our Elasticsearch container. 
 # That means we can access to elasticsearch inside a container with the url "elasticsearchgrotto:9200".
+isLSContainerExisting="$(docker ps --all --quiet --filter=name="$LS_TAGNAME")"
+if [ -n "$isLSContainerExisting" ]; then
+  echo "### DELETING old Logstash Container ###"
+  docker stop $LS_TAGNAME && docker rm -f $LS_TAGNAME
+fi
+
 echo "### BUILD the Logstash container ###"
-docker rm -f ${LS_TAGNAME}
 docker run --rm -d \
     --name ${LS_TAGNAME} \
     --health-cmd='curl -X GET "localhost:9600" --silent' \
@@ -132,23 +142,12 @@ docker run --rm -d \
 waitContainer ${LS_TAGNAME}
 echo "### Logstash available and Data IS LOADING into ${ES_TAGNAME} ###"
 
-echo "### WARNING, depending on your data this step can take more than 5 minutes ! ###"
+echo "### WARNING, depending on your data this step can take 5-10 minutes or even more! ###"
 echo "### Note that even if the deployment process is finished, the indexation will continue in the background. ###"
- 
-echo "### RUN grottocenter image locally ###"
-# Here with sails_models__connection we override the production models connection to use the dev database
-docker rm -f ${GC_TAGNAME}
-docker run -d \
-    -p ${GC_LOCAL_PORT}:8080 \
-    --restart=always \
-    -e sails_models__connection=grottoceMysqlLocalDocker \
-    -e sails_appUrl='http://localhost:${GC_LOCAL_PORT}' \
-    --name=${GC_TAGNAME} \
-    --link ${MYSQL_TAGNAME} \
-    --link ${ES_TAGNAME} \
-    ${GC_TAGNAME}
-
-echo "########################################"
-echo "### End of the deployment process - Grottocenter available on port ${GC_LOCAL_PORT} ###"
-
-
+echo "### You can go to http://localhost:9200/_cat/indices?v to see the number of documents indexed."
+ echo ""
+echo "####################################################"
+echo "#### Grottocenter development environment ready ####"
+echo "####################################################"
+echo ""
+echo "Run 'npm run start-hot' command to launch the server in live reloading mode! Happy coding :)"
