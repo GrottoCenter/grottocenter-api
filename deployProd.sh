09d12cdc5b28dfbadbadc2ac762e7f295b854d22
Lucas Goncalves <lucas.pro@mail.com>
2020-02-05 15:59:06 +0100

    feat(devops): setup application to be deployed on AWS

--- deployProd.sh
+++ deployProd.sh
@@ -1,4 +1,43 @@
 #!/usr/bin/env bash
 # Author: Clément Roig - Godefroi Roussel - Wikicaves
-# Date: Jan 2019
-# Requirement: docker
+# Contributors: Lucas Gonçalves
+# Date: Jan 2019 (v2 Feb 2020)
+. /home/ec2-user/.nvm/nvm.sh
+. /home/ec2-user/.bashrc
+
+
+cd /home/ec2-user/GrottoCenter3
+
+#  ============= FIRST DEPLOYEMENT ONLY /!\ ============= 
+#
+#   Check prerequisites for development:
+#   npm install -g grunt-cli
+#   sudo amazon-linux-extras install nginx1.12
+#
+#  ======================================================
+
+# Install dependencies
+# Fix for missing depencie
+
+sudo chown -R ec2-user /home/ec2-user/GrottoCenter3/
+
+# Important to keep this version
+nvm use 10.4.1
+
+npm install grunt-cli
+npm run-script build
+npm install --production --unsafe-perm || \
+  ((if [ -f npm-debug.log ]; then \
+      cat npm-debug.log; \
+    fi) && false)
+
+# Install files from private bucket
+aws s3 cp  s3://appgrottocenter3/production.js /home/ec2-user/GrottoCenter3/config/env/production.js
+aws s3 cp  s3://appgrottocenter3/env /home/ec2-user/GrottoCenter3/.env
+aws s3 cp  s3://appgrottocenter3/transifexrc /home/ec2-user/GrottoCenter3/.transifexrcgit
+
+NODE_ENV=production sails_hooks__grunt=false nohup node app.js --production  > /dev/null 2> /dev/null < /dev/null &
+
+
+
+
