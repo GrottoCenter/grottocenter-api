c03bb266cbd2bba64ad914bb162513e581cf80ba
Lucas Goncalves <lucas.pro@mail.com>
2020-02-05 16:51:21 +0100

    feat(devops): change deploy after install and not during install

--- deployProd.sh
+++ deployProd.sh
@@ -36,8 +36,4 @@ aws s3 cp  s3://appgrottocenter3/production.js /home/ec2-user/GrottoCenter3/conf
 aws s3 cp  s3://appgrottocenter3/env /home/ec2-user/GrottoCenter3/.env
 aws s3 cp  s3://appgrottocenter3/transifexrc /home/ec2-user/GrottoCenter3/.transifexrcgit
 
-NODE_ENV=production sails_hooks__grunt=false nohup node app.js --production  > /dev/null 2> /dev/null < /dev/null &
-
-
-
 
