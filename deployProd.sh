1bfce7e83b9e83a63eb4f3e6f3640bdbde09398a
Fatima Machhouri <32356466+FatimaMachhouri@users.noreply.github.com>
2020-03-16 02:21:07 +0100

    Create deployProd.sh

---
+++ deployProd.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# Author: Clément Roig - Godefroi Roussel - Wikicaves
+# Contributors: Lucas Gonçalves
+# Date: Jan 2019 (v2 Feb 2020)
+. /home/ec2-user/.nvm/nvm.sh
+. /home/ec2-user/.bashrc
+
+
+cd /home/ec2-user/GrottoCenter3
+
+#  ============= FIRST DEPLOYEMENT ONLY /!\ ============= 
+#
+#   Check prerequisites for development:
+  npm install -g grunt-cli
+#   sudo amazon-linux-extras install nginx1.12
+#   npm install forever -g
+#
+#  ======================================================
+
+# Install dependencies
+# Fix for missing depencie
+
+sudo chown -R ec2-user /home/ec2-user/GrottoCenter3/
+
+# Important to keep this version
+nvm use 10.4.1
+
+npm install grunt-cli
+npm run-script build
+npm install --production --unsafe-perm || \
+  ((if [ -f npm-debug.log ]; then \
+      cat npm-debug.log; \
+    fi) && false)
+
+# Install files from private bucket
+aws s3 cp  s3://appgrottocenter3/production.js /home/ec2-user/GrottoCenter3/config/env/production.js
+aws s3 cp  s3://appgrottocenter3/env /home/ec2-user/GrottoCenter3/.env
+aws s3 cp  s3://appgrottocenter3/transifexrc /home/ec2-user/GrottoCenter3/.transifexrc
+
+NODE_ENV=production sails_hooks__grunt=false nohup node app.js --production > log.txt
+
+
+
+
