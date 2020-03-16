3a85734a8ca2f31ea2e94053f5acc9201e91dd4b
Fatima Machhouri <32356466+FatimaMachhouri@users.noreply.github.com>
2020-03-16 01:56:48 +0100

    Update landing.jsx

--- assets/js/react/components/pages/Landing.jsx
+++ assets/js/react/components/pages/Landing.jsx
@@ -1,4 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import SideMenu from '../common/SideMenu/SideMenu';
 
 import BasePage from './BasePage';
 import Header from '../homepage/Header';
@@ -13,23 +24,137 @@ import GrottoAppBar from '../common/GrottoAppBar';
 
 //
 //
+// S T Y L I N G - C O M P O N E N T S
+//
+//
+
+const drawerWidth = 240;
+
+const styles = (theme) => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  menuButton: {
+    marginRight: 20,
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+  },
+});
+
+//
+//
 // M A I N - C O M P O N E N T
 //
 //
-const Landing = () => (
-  <BasePage>
-    <div id="landingpage">
-      <GrottoAppBar />
-      <Header />
-      <Welcome />
-      <WhatIsIt />
-      <RandomEntry />
-      <LatestBlogNewsSection />
-      <Association />
-      <PartnersSection />
-      <Footer />
-    </div>
-  </BasePage>
-);
 
-export default Landing;
+class Landing extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      mobileOpen: false,
+    };
+  }
+
+  handleDrawerToggle = () => {
+    this.setState((state) => ({ mobileOpen: !this.state.mobileOpen }));
+  };
+
+  render() {
+    const { classes, theme } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={this.handleDrawerToggle}
+              className={classes.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" noWrap>
+              GrottoCenter
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <nav className={classes.drawer}>
+          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+          <Hidden smUp implementation="css">
+            <Drawer
+              container={this.props.container}
+              variant="temporary"
+              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+              open={this.state.mobileOpen}
+              onClose={this.handleDrawerToggle}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+            >
+              <SideMenu />
+            </Drawer>
+          </Hidden>
+          <Hidden xsDown implementation="css">
+            <Drawer
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              variant="permanent"
+              open
+            >
+              <SideMenu />
+            </Drawer>
+          </Hidden>
+        </nav>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <BasePage>
+            <div id="landingpage">
+              <GrottoAppBar />
+              <Header />
+              <Welcome />
+              <WhatIsIt />
+              <RandomEntry />
+              <LatestBlogNewsSection />
+              <Association />
+              <PartnersSection />
+              <Footer />
+            </div>
+          </BasePage>
+        </main>
+      </div>
+    );
+  }
+}
+
+Landing.propTypes = {
+  classes: PropTypes.object.isRequired,
+  // Injected by the documentation to work in an iframe.
+  // You won't need it on your project.
+  container: PropTypes.object,
+  theme: PropTypes.object.isRequired
+};
+
+export default withStyles(styles, { withTheme: true })(Landing);
