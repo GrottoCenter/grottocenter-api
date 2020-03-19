a71c15104cc02d16935533a4dbb8386d689a605d
Lucas Goncalves <lucas.pro@mail.com>
2020-03-19 17:39:14 +0100

    feat(devops): change echo

--- deployProd.sh
+++ deployProd.sh
@@ -27,7 +27,7 @@ cd /home/ec2-user/GrottoCenter3
 sudo chown -R ec2-user /home/ec2-user/GrottoCenter3/
 
 # Important to keep this version
-echo("# ========== Installation des dépendances")
+echo "# ========== Installation des dépendances" 
 
 
 npm install grunt-cli
@@ -38,13 +38,14 @@ npm install --production --unsafe-perm || \
     fi) && false)
 
 # Install files from private bucket
-echo("# ========== Récupération des fichiers privés")
+echo "# ========== Récupération des fichiers privés"
 
 aws s3 cp  s3://appgrottocenter3/production.js /home/ec2-user/GrottoCenter3/config/env/production.js
 aws s3 cp  s3://appgrottocenter3/env /home/ec2-user/GrottoCenter3/.env
 aws s3 cp  s3://appgrottocenter3/transifexrc /home/ec2-user/GrottoCenter3/.transifexrc
 
 
-echo("# ========== Lancement de l'application")
+echo "# ========== Lancement de l'application"
 NODE_ENV=production sails_hooks__grunt=false nohup node app.js --production > log.txt
 
+echo "# ========== Lancement terminé"
