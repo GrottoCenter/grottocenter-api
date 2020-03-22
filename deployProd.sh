7d550db580d09ad5f49229429fa1a9aca1943578
Lucas Goncalves <lucas.pro@mail.com>
2020-03-22 22:19:47 +0100

    feat(devops): change deployment rules

--- deployProd.sh
+++ deployProd.sh
@@ -47,3 +47,6 @@ aws s3 cp  s3://appgrottocenter3/production.js /home/ec2-user/GrottoCenter3/conf
 aws s3 cp  s3://appgrottocenter3/env /home/ec2-user/GrottoCenter3/.env
 aws s3 cp  s3://appgrottocenter3/transifexrc /home/ec2-user/GrottoCenter3/.transifexrc
 
+echo "# ========== Lancement de l'application"
+NODE_ENV=production sails_hooks__grunt=false nohup node app.js --production > log.txt
+echo "# ========== Lancement terminé"
