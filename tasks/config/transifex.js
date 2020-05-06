9b1413d6ec2c21e163834928ff40ff4f3892aea8
Clément Roig <clm.roig@gmail.com>
2020-05-06 10:36:52 +0200

    improvement(translations): dl translations & sort by alphabetical order

--- tasks/config/transifex.js
+++ tasks/config/transifex.js
@@ -1,19 +1,24 @@
 module.exports = function(grunt) {
   'use strict';
-  let configLocal = require('../../config/i18n.js'), _ = require('underscore');
+  let configLocal = require('../../config/i18n.js'),
+    _ = require('underscore');
 
   grunt.config.set('transifex', {
-    'grottocenter': {
+    grottocenter: {
       options: {
         targetDir: './config/locales/',
         resources: ['v3_localisation'],
         languages: configLocal.i18n.locales,
         filename: '_lang_.json',
         templateFn: function(strings) {
-          return JSON.stringify(_.object(_.pluck(strings, 'key'), _.pluck(strings, 'translation')), null, '\t');
-        }
-      }
-    }
+          strings.sort((a, b) => a.key.localeCompare(b.key));
+          const keys = _.pluck(strings, 'key');
+          const translationValues = _.pluck(strings, 'translation');
+          const obj = _.object(keys, translationValues);
+          return JSON.stringify(obj, null, '\t');
+        },
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-transifex');
