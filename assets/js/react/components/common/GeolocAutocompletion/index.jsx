ea8e77337243fa4696a3138f75f54ea6be873158
ArthurLeblanc <arthurleblanc11@gmail.com>
2020-06-02 18:21:30 +0200

    improvement(geolocExample): code refactored & hooks implementation

--- assets/js/react/components/common/GeolocAutocompletion/index.jsx
+++ assets/js/react/components/common/GeolocAutocompletion/index.jsx
@@ -2,92 +2,130 @@ import * as React from 'react';
 import useLocationPredictions from '../../../hooks/useLocationPredictions';
 import usePlacesDetail from '../../../hooks/usePlacesDetail';
 
+const sessionToken =
+  Math.random()
+    .toString(36)
+    .substring(2, 15) +
+  Math.random()
+    .toString(36)
+    .substring(2, 15);
+
 /**
  * Example of gelocalisation autocomplete on all fields and translation gestion
  */
 const example = () => {
   // contryCode : needed to restric the localization for the region field
-  // input** : Value retrieved when the user is typing into the input.
-  // **Code : ID of Place Object. Needed for translation and place restriction.
+  // input : Value retrieved when the user is typing into the input.
+  // id : ID of Place Object. Needed for translation and place restriction.
   // **Bounds : Google Bounds Object, needed for the localization restriction.
   // **Translated : Name of the fields translated with countryCode.
 
-  const [inputCountry, setInputCountry] = React.useState('');
-  const [countryId, setCountryId] = React.useState('');
-  const countryCode = usePlacesDetail(countryId);
+  const [formValue, setFormValue] = React.useState({
+    country: { id: '', input: '' },
+    region: { id: '', input: '' },
+    county: { id: '', input: '' },
+    city: { id: '', input: '' },
+  });
 
-  const [inputRegion, setInputRegion] = React.useState('');
-  const [regionId, setRegionId] = React.useState('');
-  const regionBounds = usePlacesDetail(regionId, countryCode);
+  const countryCode = usePlacesDetail(formValue.country.id, null, sessionToken);
+  const regionBounds = usePlacesDetail(
+    formValue.region.id,
+    countryCode,
+    sessionToken,
+  );
+  const countyBounds = usePlacesDetail(
+    formValue.county.id,
+    countryCode,
+    sessionToken,
+  );
 
-  const [inputCounty, setInputCounty] = React.useState('');
-  const [countyId, setCountyId] = React.useState('');
-  const countyBounds = usePlacesDetail(countyId, countryCode);
-
-  const [inputCity, setInputCity] = React.useState('');
-  const [cityId, setCityId] = React.useState('');
-
-  const regionTranslated = usePlacesDetail(regionId, 'ru');
-  const countyTranslated = usePlacesDetail(countyId, 'ru');
-  const cityTranslated = usePlacesDetail(cityId, 'ru');
+  const regionTranslated = usePlacesDetail(
+    formValue.region.id,
+    countryCode,
+    sessionToken,
+  );
+  const countyTranslated = usePlacesDetail(
+    formValue.county.id,
+    countryCode,
+    sessionToken,
+  );
+  const cityTranslated = usePlacesDetail(
+    formValue.city.id,
+    countryCode,
+    sessionToken,
+  );
 
   // Hook to enable autocomplete on a field
   const predictionsCountry = useLocationPredictions(
-    inputCountry,
+    formValue.country.input,
     'country',
-    null,
+    sessionToken,
     null,
   );
   const predictionsRegions = useLocationPredictions(
-    inputRegion,
+    formValue.region.input,
     'region',
+    sessionToken,
     countryCode,
     null,
   );
   const predictionsCounty = useLocationPredictions(
-    inputCounty,
+    formValue.county.input,
     'county',
+    sessionToken,
     null,
     regionBounds[1],
   );
   const predictionsCity = useLocationPredictions(
-    inputCity,
+    formValue.city.input,
     'city',
+    sessionToken,
     null,
     countyBounds[1],
   );
 
-  // Triggered when a prediction is selected, retrieves place's detail
-  const onSelect = (prediction, type) => {
-    if (prediction) {
-      if (type === 'country') {
-        setInputCountry(prediction.label);
-        setCountryId(prediction.id);
-      } else if (type === 'region') {
-        setInputRegion(prediction.label);
-        setRegionId(prediction.id);
-      } else if (type === 'county') {
-        setInputCounty(prediction.label);
-        setCountyId(prediction.id);
-      } else if (type === 'city') {
-        setInputCity(prediction.label);
-        setCityId(prediction.id);
-      }
+  // Sets state value (id & label) of the specified type
+  const handleFormChange = (field, type) => {
+    switch (type) {
+      case 'country':
+        setFormValue({ ...formValue, country: field });
+        break;
+      case 'region':
+        setFormValue({ ...formValue, region: field });
+        break;
+      case 'county':
+        setFormValue({ ...formValue, county: field });
+        break;
+      case 'city':
+        setFormValue({ ...formValue, city: field });
+        break;
+      default:
+        break;
     }
   };
 
   return (
     <div>
       <input
-        value={inputCountry}
-        onChange={(event) => setInputCountry(event.target.value)}
+        value={formValue.country.input}
+        onChange={(event) =>
+          setFormValue({
+            ...formValue,
+            country: { id: '', input: event.target.value },
+          })
+        }
       />
       <ul>
         {predictionsCountry.map((prediction) => (
           <li
+            key={prediction.id}
             role="presentation"
-            onClick={() => onSelect(prediction, 'country')}
-            onKeyPress={() => onSelect(prediction, 'country')}
+            onClick={() =>
+              handleFormChange(
+                { id: prediction.id, input: prediction.label },
+                'country',
+              )
+            }
           >
             {prediction.label}
           </li>
@@ -95,14 +133,24 @@ const example = () => {
       </ul>
 
       <input
-        value={inputRegion}
-        onChange={(event) => setInputRegion(event.target.value)}
+        value={formValue.region.input}
+        onChange={(event) =>
+          setFormValue({
+            ...formValue,
+            region: { id: '', input: event.target.value },
+          })
+        }
       />
       <ul>
         {predictionsRegions.map((prediction) => (
           <li
             role="presentation"
-            onClick={() => onSelect(prediction, 'region')}
+            onClick={() =>
+              handleFormChange(
+                { id: prediction.id, input: prediction.label },
+                'region',
+              )
+            }
             key={prediction.id}
           >
             {prediction.label}
@@ -111,14 +159,24 @@ const example = () => {
       </ul>
 
       <input
-        value={inputCounty}
-        onChange={(event) => setInputCounty(event.target.value)}
+        value={formValue.county.input}
+        onChange={(event) =>
+          setFormValue({
+            ...formValue,
+            county: { id: '', input: event.target.value },
+          })
+        }
       />
       <ul>
         {predictionsCounty.map((prediction) => (
           <li
             role="presentation"
-            onClick={() => onSelect(prediction, 'county')}
+            onClick={() =>
+              handleFormChange(
+                { id: prediction.id, input: prediction.label },
+                'county',
+              )
+            }
             key={prediction.id}
           >
             {prediction.label}
@@ -127,14 +185,24 @@ const example = () => {
       </ul>
 
       <input
-        value={inputCity}
-        onChange={(event) => setInputCity(event.target.value)}
+        value={formValue.city.input}
+        onChange={(event) =>
+          setFormValue({
+            ...formValue,
+            city: { id: '', input: event.target.value },
+          })
+        }
       />
       <ul>
         {predictionsCity.map((prediction) => (
           <li
             role="presentation"
-            onClick={() => onSelect(prediction, 'city')}
+            onClick={() =>
+              handleFormChange(
+                { id: prediction.id, input: prediction.label },
+                'city',
+              )
+            }
             key={prediction.id}
           >
             {prediction.label}
@@ -145,10 +213,10 @@ const example = () => {
       <div className="results">
         <h2>Translated fields</h2>
         <ul>
-          <li>Country : {countryCode}</li>
-          <li>Region : {regionTranslated[0]}</li>
-          <li>County : {countyTranslated[0]}</li>
-          <li>City : {cityTranslated[0]}</li>
+          <li id="countryTranslated">Country : {countryCode}</li>
+          <li id="regionTranslated">Region : {regionTranslated[0]}</li>
+          <li id="countyTranslated">County : {countyTranslated[0]}</li>
+          <li id="cityTranslated">City : {cityTranslated[0]}</li>
         </ul>
       </div>
     </div>
