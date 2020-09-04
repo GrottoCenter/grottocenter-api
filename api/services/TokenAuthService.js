c22f85ccee777eb48596d1c2a8d42bdb16d0754f
Clément Roig <clm.roig@gmail.com>
2020-09-04 15:10:54 +0200

    improvement(authToken): use prod token salt if available

--- api/services/TokenAuthService.js
+++ api/services/TokenAuthService.js
@@ -7,13 +7,15 @@
 
 const jwt = require('jsonwebtoken');
 
-const tokenSecret = '54ff54rfyTE5656skfopkse564867fdcd54cdc534ef4se';
+const tokenSalt = process.env.TOKEN_SALT
+  ? process.env.TOKEN_SALT
+  : 'aR4nd0mT0kenSalt';
 
 // Generates a token from supplied payload
 module.exports.issue = (payload) =>
   jwt.sign(
     payload,
-    tokenSecret, // Token Secret that we sign it with
+    tokenSalt, // Token Secret that we sign it with
     {
       expiresIn: 60 * 60 * 24,
     },
@@ -23,7 +25,7 @@ module.exports.issue = (payload) =>
 module.exports.verify = (token, callback) =>
   jwt.verify(
     token, // The token to be verified
-    tokenSecret, // Same token we used to sign
+    tokenSalt, // Same token we used to sign
     {}, // No Option, for more see https://github.com/auth0/node-jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
     callback, // Pass errors or decoded token to callback
   );
