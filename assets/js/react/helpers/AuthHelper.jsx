6540b3e2a827a63160d2a2b314da1846126354a0
Clément Roig <clm.roig@gmail.com>
2020-09-16 11:51:09 +0200

    fix(authSystem): refactor and cleaning
    - Removing authToken is now performed in reducer (instead of in action)
    - Don't redirect to home page when logout => components must handle the disconnection by
    themselves
    - Add some useful features to the AuthHelper
    - Adapt the DocumentSubmission component to this new mechanisms

--- assets/js/react/helpers/AuthHelper.jsx
+++ assets/js/react/helpers/AuthHelper.jsx
@@ -1,6 +1,38 @@
+import { decode } from 'jsonwebtoken';
 import { identificationTokenName } from '../conf/Config';
 
+// =========================================
+
+export const getAuthToken = () => {
+  return window.localStorage.getItem(identificationTokenName);
+};
+
+/**
+ * True if the user has a group with the name "Administrator", else false.
+ */
+export const isAdmin = () => {
+  const authToken = getAuthToken();
+  if (authToken === null) return false;
+  const decodedToken = decode(authToken);
+  return decodedToken.groups.some((g) => g.name === 'Administrator');
+};
+
+export const isTokenExpired = () => {
+  try {
+    const decoded = decode(getAuthToken());
+    if (decoded.exp < Date.now() / 1000) {
+      return true;
+    }
+    return false;
+  } catch (err) {
+    return false;
+  }
+};
+
+export const removeAuthToken = () => {
+  window.localStorage.removeItem(identificationTokenName);
+};
+
 export default function isAuth() {
-  // Just check if there is a token
-  return window.localStorage.getItem(identificationTokenName) != null;
+  return getAuthToken() != null;
 }
