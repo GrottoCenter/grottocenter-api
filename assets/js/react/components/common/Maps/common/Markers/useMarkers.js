3a5134c74eb873ef7c3fb6303aa336d731a68b7d
Maxime Soufflet <maximenathan@gmail.com>
2020-12-03 16:36:43 +0100

    fix(map): css breaking when display markers

--- assets/js/react/components/common/Maps/common/Markers/useMarkers.js
+++ assets/js/react/components/common/Maps/common/Markers/useMarkers.js
@@ -1,9 +1,15 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useMap } from 'react-leaflet';
 import * as L from 'leaflet';
 import { anyPass, forEach, isEmpty, isNil, map as rMap } from 'ramda';
 import { renderToString } from 'react-dom/server';
-import { createGlobalStyle, keyframes } from 'styled-components';
+import {
+  createGlobalStyle,
+  keyframes,
+  ThemeProvider as StyledThemeProvider,
+} from 'styled-components';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import theme from '../../../../../conf/grottoTheme';
 
 const isNilOrEmpty = anyPass([isNil, isEmpty]);
 
@@ -32,7 +38,18 @@ const useMarkers = (icon, popupContent = null, tooltipContent = null) => {
     if (!isNil(popupContent)) {
       if (!isNil(tooltipContent)) {
         return L.marker([latitude, longitude], options)
-          .bindPopup(renderToString(popupContent(marker)))
+          .bindPopup(
+            renderToString(
+              // Without theme provider the CSS doesn't work properly
+              // It's makes the map slower when there is a lot of markers
+              // One way to optimize it would be to not use MUI for the markers
+              <StyledThemeProvider theme={theme}>
+                <MuiThemeProvider theme={theme}>
+                  {popupContent(marker)}
+                </MuiThemeProvider>
+              </StyledThemeProvider>,
+            ),
+          )
           .bindTooltip(tooltipContent(marker), { direction: 'bottom' });
       }
       return L.marker([latitude, longitude], options).bindPopup(
