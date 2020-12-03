427b4979588b2e23446b1738d2efc6588e27434a
Maxime Soufflet <maximenathan@gmail.com>
2020-12-03 18:19:39 +0100

    refactor(map): refactor markers tooltip styles and add story

--- assets/js/react/components/common/Maps/common/Markers/Components/OrganizationPopup.jsx
+++ assets/js/react/components/common/Maps/common/Markers/Components/OrganizationPopup.jsx
@@ -1,72 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import DescriptionIcon from '@material-ui/icons/Description';
-import LocationCityIcon from '@material-ui/icons/LocationCity';
-import PlaceIcon from '@material-ui/icons/Place';
-import { withTheme } from '@material-ui/core';
-import GCLink from '../../../../GCLink';
+import { isNil } from 'ramda';
+import { GpsFixed, Public } from '@material-ui/icons';
+import Title from './Title';
+import { Property } from '../../../../Properties';
+import { makeCoordinatesValue } from './utils';
 
-const StyledTitle = styled.h5`
-  text-align: center;
-`;
-
-const MainDiv = styled.div`
-  display: table-row;
-  margin-bottom: 10px;
-`;
-
-const SubDiv = styled.div`
-  display: table-cell;
-  vertical-align: middle;
-  padding-bottom: 10px;
-  font-size: small;
-`;
-
-const StyledDescriptionIcon = styled(DescriptionIcon)`
-  vertical-align: middle;
-`;
-
-const StyledPlaceIcon = withTheme(styled(PlaceIcon)`
-  color: ${(props) => props.theme.palette.secondary4Color};
-`);
-
-const StyledLocationCityIcon = withTheme(styled(LocationCityIcon)`
-  color: ${(props) => props.theme.palette.secondary4Color};
-`);
-
-export const OrganizationPopup = ({ organization }) => {
-  const grottoDetailedPageLink = `/ui/organizations/${organization.id}`;
-
-  return (
-    <>
-      <div>
-        <GCLink internal={false} href={grottoDetailedPageLink} target="_blank">
-          <StyledTitle>
-            {organization.name}
-            <StyledDescriptionIcon />
-          </StyledTitle>
-        </GCLink>
-        {organization.address && (
-          <MainDiv>
-            <StyledLocationCityIcon />
-            <SubDiv>{organization.address}</SubDiv>
-          </MainDiv>
-        )}
-        <MainDiv>
-          <StyledPlaceIcon />
-          <SubDiv>
-            {'Lat : '}
-            {organization.latitude.toFixed(6)}
-            <br />
-            {'Lng : '}
-            {organization.longitude.toFixed(6)}
-          </SubDiv>
-        </MainDiv>
-      </div>
-    </>
-  );
-};
+export const OrganizationPopup = ({ organization }) => (
+  <>
+    <Title
+      title={organization.name && organization.name.toUpperCase()}
+      link={`/ui/groups/${organization.id}`}
+    />
+    {!isNil(organization.address) && (
+      <Property
+        secondary
+        value={organization.address}
+        icon={<Public color="primary" />}
+      />
+    )}
+    <Property
+      secondary
+      value={makeCoordinatesValue(
+        organization.latitude,
+        organization.longitude,
+      )}
+      icon={<GpsFixed color="primary" />}
+    />
+  </>
+);
 
 OrganizationPopup.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
