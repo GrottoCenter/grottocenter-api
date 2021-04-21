1e4fc182ccd5509476803f2ea311025cbf8ddf86
Clément Roig <clm.roig@gmail.com>
2021-04-21 15:52:36 +0200

    fix(caverApi): fix right action to use to create a caver

--- api/controllers/CaverController.js
+++ api/controllers/CaverController.js
@@ -320,7 +320,7 @@ module.exports = {
       .with({
         groups: req.token.groups,
         rightEntity: RightService.RightEntities.CAVER,
-        rightAction: RightService.RightActions.EDIT_ANY,
+        rightAction: RightService.RightActions.CREATE,
       })
       .intercept('rightNotFound', (err) => {
         return res.serverError(
