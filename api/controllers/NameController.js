1336f95536452dfef7ca9841a5fd6a44553d5698
Clément Roig <clm.roig@gmail.com>
2021-05-14 10:16:36 +0200

    fix(nameApi): use setAsMain instead of setIsMain

--- api/controllers/NameController.js
+++ api/controllers/NameController.js
@@ -8,7 +8,7 @@
 module.exports = {
   update: async (req, res, converter) => {
     // Check right
-    const hasRightOnAny = await sails.helpers.checkRight
+    const hasRight = await sails.helpers.checkRight
       .with({
         groups: req.token.groups,
         rightEntity: RightService.RightEntities.NAME,
@@ -20,17 +20,9 @@ module.exports = {
         );
       });
 
-    const hasRightOnOwn = await sails.helpers.checkRight
-      .with({
-        groups: req.token.groups,
-        rightEntity: RightService.RightEntities.NAME,
-        rightAction: RightService.RightActions.EDIT_OWN,
-      })
-      .intercept('rightNotFound', (err) => {
-        return res.serverError(
-          'A server error occured when checking your right to update your names.',
-        );
-      });
+    if (!hasRight) {
+      return res.forbidden('You are not authorized to update any name.');
+    }
 
     // Check if name exists
     const nameId = req.param('id');
@@ -43,19 +35,7 @@ module.exports = {
 
     const cleanedData = {
       name: req.param('name'),
-      isMain: req.param('isMain'),
     };
-
-    // Check right on this particular name
-    if (!hasRightOnAny) {
-      if (hasRightOnOwn) {
-        if (req.token.id !== currentName.author) {
-          return res.forbidden("You can not update a name you didn't created.");
-        }
-      } else {
-        return res.forbidden('You can not update a name.');
-      }
-    }
 
     // Launch update request
     const updatedName = await TName.updateOne({
@@ -101,9 +81,9 @@ module.exports = {
     );
   },
 
-  updateIsMain: async (req, res, converter) => {
+  setAsMain: async (req, res, converter) => {
     // Check right
-    const hasRightOnAny = await sails.helpers.checkRight
+    const hasRight = await sails.helpers.checkRight
       .with({
         groups: req.token.groups,
         rightEntity: RightService.RightEntities.NAME,
@@ -114,6 +94,9 @@ module.exports = {
           'A server error occured when checking your right to update any name.',
         );
       });
+    if (!hasRight) {
+      return res.forbidden('You are not authorized to update any name.');
+    }
 
     // Check if name exists
     const nameId = req.param('id');
@@ -124,10 +107,17 @@ module.exports = {
       });
     }
 
-    const newIsMainValue = req.param('isMain');
-    if (newIsMainValue !== true && newIsMainValue !== false) {
-      return res.badRequest(
-        `You must provide an isMain value ('true' or 'false').`,
+    // Do nothing if already main
+    if (currentName.isMain) {
+      const params = {};
+      params.controllerMethod = 'NameController.setAsMain';
+      return ControllerService.treatAndConvert(
+        req,
+        null,
+        currentName,
+        params,
+        res,
+        converter,
       );
     }
 
@@ -135,7 +125,7 @@ module.exports = {
     const updatedName = await TName.updateOne({
       id: nameId,
     })
-      .set({ isMain: newIsMainValue })
+      .set({ isMain: true })
       .intercept('E_UNIQUE', (e) => {
         sails.log.error(e.message);
         return res.status(409).send(e.message);
@@ -153,38 +143,36 @@ module.exports = {
         return res.serverError(e.message);
       });
 
-    // If it's true, update other names of the entity to isMain = false
-    if (newIsMainValue === true) {
-      const possibleEntites = [
-        { id: updatedName.cave, type: 'cave' },
-        { id: updatedName.entrance, type: 'entrance' },
-        { id: updatedName.grotto, type: 'grotto' },
-        { id: updatedName.massif, type: 'massif' },
-        { id: updatedName.point, type: 'point' },
-      ];
-      const entity = possibleEntites.find((e) => e.id !== null);
-      await TName.update({
-        [entity.type]: entity.id,
-        id: { '!=': updatedName.id },
+    // Update other names of the entity to isMain = false
+    const possibleEntites = [
+      { id: updatedName.cave, type: 'cave' },
+      { id: updatedName.entrance, type: 'entrance' },
+      { id: updatedName.grotto, type: 'grotto' },
+      { id: updatedName.massif, type: 'massif' },
+      { id: updatedName.point, type: 'point' },
+    ];
+    const entity = possibleEntites.find((e) => e.id !== null);
+    await TName.update({
+      [entity.type]: entity.id,
+      id: { '!=': updatedName.id },
+    })
+      .set({ isMain: false })
+      .intercept('E_UNIQUE', (e) => {
+        sails.log.error(e.message);
+        return res.status(409).send(e.message);
       })
-        .set({ isMain: false })
-        .intercept('E_UNIQUE', (e) => {
-          sails.log.error(e.message);
-          return res.status(409).send(e.message);
-        })
-        .intercept({ name: 'UsageError' }, (e) => {
-          sails.log.error(e.message);
-          return res.badRequest(e.message);
-        })
-        .intercept({ name: 'AdapterError' }, (e) => {
-          sails.log.error(e.message);
-          return res.badRequest(e.message);
-        })
-        .intercept((e) => {
-          sails.log.error(e.message);
-          return res.serverError(e.message);
-        });
-    }
+      .intercept({ name: 'UsageError' }, (e) => {
+        sails.log.error(e.message);
+        return res.badRequest(e.message);
+      })
+      .intercept({ name: 'AdapterError' }, (e) => {
+        sails.log.error(e.message);
+        return res.badRequest(e.message);
+      })
+      .intercept((e) => {
+        sails.log.error(e.message);
+        return res.serverError(e.message);
+      });
 
     // Return name updated and populated
     const newName = await TName.findOne(nameId)
@@ -198,7 +186,7 @@ module.exports = {
       .populate('reviewer');
 
     const params = {};
-    params.controllerMethod = 'NameController.updateIsMain';
+    params.controllerMethod = 'NameController.setAsMain';
     return ControllerService.treatAndConvert(
       req,
       null,
