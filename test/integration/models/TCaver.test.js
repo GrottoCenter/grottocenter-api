a38cecb80fd9685e4fca8bfd5495d7e810f53e4e
Clément Roig <clm.roig@gmail.com>
2021-09-28 13:11:28 +0200

    test(update): make old tests to work with new tools

--- test/integration/models/TCaver.test.js
+++ test/integration/models/TCaver.test.js
@@ -1,13 +1,18 @@
 let should = require('should');
+const Fixted = require('fixted');
+const fixted = new Fixted();
+const fixtures = fixted.data;
 
-/*describe('TCaverModel', function() {
-
+describe('TCaverModel', function() {
   describe('ORM -> find all', function() {
-    it('should check find function', function (done) {
-      TCaver.find({})
+    it('should check find all function', function(done) {
+      TCaver.find()
         .then(function(results) {
-          fixtures.tcaver.length.should.be.greaterThan(0);
-          this.should.equal(results.length, results.length, 'result size must be equals to ' + fixtures.tcaver.length);
+          this.should.equal(
+            results.length,
+            fixtures.tcaver.length,
+            'result size must be equals to ' + fixtures.tcaver.length,
+          );
           done();
         })
         .catch((err) => done(err));
@@ -15,10 +20,9 @@ let should = require('should');
   });
 
   describe('ORM -> find by good login', function() {
-    it('should check find function', function (done) {
+    it('should check find function', function(done) {
       TCaver.find({ login: 'test2' })
         .then(function(results) {
-          fixtures.tcaver.length.should.be.greaterThan(0);
           results.length.should.be.equal(1);
           done();
         })
@@ -27,15 +31,13 @@ let should = require('should');
   });
 
   describe('ORM -> find by bad login', function() {
-    it('should check find function', function (done) {
+    it('should check find function', function(done) {
       TCaver.find({ login: 'bad_login' })
         .then(function(results) {
-          fixtures.tcaver.length.should.be.greaterThan(0);
           results.length.should.be.equal(0);
           done();
         })
         .catch((err) => done(err));
     });
   });
-
-});*/
+});
