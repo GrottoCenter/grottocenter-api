f419f7cfb8fcb546e9f723c8d2668b23d0a5f35b
Clément Roig <clm.roig@gmail.com>
2021-10-13 11:10:47 +0200

    fix(sailsDeprecatedFunction): replace jsonx() by json()
    jsonx() is deprecated : https://sailsjs.com/documentation/upgrading/to-v-1-0#?response-methods

--- api/responses/forbidden.js
+++ api/responses/forbidden.js
@@ -35,7 +35,7 @@ module.exports = function forbidden(data, options) {
 
   // If the user-agent wants JSON, always respond with JSON
   if (req.wantsJSON) {
-    return res.jsonx(data);
+    return res.json(data);
   }
 
   // If second argument is a string, we take that to mean it refers to a view.
@@ -83,7 +83,7 @@ module.exports = function forbidden(data, options) {
               err,
             );
           }
-          return res.jsonx(data);
+          return res.json(data);
         }
 
         return res.send(html);
