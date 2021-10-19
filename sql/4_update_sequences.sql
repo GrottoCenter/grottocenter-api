691e27893dc95d92d0743a98b38202dba25e1b61
Clément Roig <clm.roig@gmail.com>
2021-10-19 16:17:15 +0200

    tech(dataSeeding): improve update seq sql code

--- sql/4_update_sequences.sql
+++ sql/4_update_sequences.sql
@@ -1,36 +1,37 @@
 \c grottoce;
+-- See this page to know how to get all the sequences updated: https://wiki.postgresql.org/wiki/Fixing_Sequences
 
 -- h_ tables
-SELECT SETVAL('h_cave_id_seq', (SELECT MAX(id) + 1 FROM public.h_cave));
-SELECT SETVAL('h_comment_id_seq', (SELECT MAX(id) + 1 FROM public.h_comment));
-SELECT SETVAL('h_description_id_seq', (SELECT MAX(id) + 1 FROM public.h_description));
-SELECT SETVAL('h_document_id_seq', (SELECT MAX(id) + 1 FROM public.h_document));
-SELECT SETVAL('h_entrance_id_seq', (SELECT MAX(id) + 1 FROM public.h_entrance));
-SELECT SETVAL('h_grotto_id_seq', (SELECT MAX(id) + 1 FROM public.h_grotto));
-SELECT SETVAL('h_history_id_seq', (SELECT MAX(id) + 1 FROM public.h_history));
-SELECT SETVAL('h_location_id_seq', (SELECT MAX(id) + 1 FROM public.h_location));
-SELECT SETVAL('h_massif_id_seq', (SELECT MAX(id) + 1 FROM public.h_massif));
-SELECT SETVAL('h_name_id_seq', (SELECT MAX(id) + 1 FROM public.h_name));
-SELECT SETVAL('h_rigging_id_seq', (SELECT MAX(id) + 1 FROM public.h_rigging));
+SELECT SETVAL('public.h_cave_id_seq', COALESCE(MAX(id), 1) ) FROM public.h_cave;
+SELECT SETVAL('public.h_comment_id_seq', COALESCE(MAX(id), 1) ) FROM public.h_comment;
+SELECT SETVAL('public.h_description_id_seq', COALESCE(MAX(id), 1) ) FROM public.h_description;
+SELECT SETVAL('public.h_document_id_seq', COALESCE(MAX(id), 1) ) FROM public.h_document;
+SELECT SETVAL('public.h_entrance_id_seq', COALESCE(MAX(id), 1) ) FROM public.h_entrance;
+SELECT SETVAL('public.h_grotto_id_seq', COALESCE(MAX(id), 1) ) FROM public.h_grotto;
+SELECT SETVAL('public.h_history_id_seq', COALESCE(MAX(id), 1) ) FROM public.h_history;
+SELECT SETVAL('public.h_location_id_seq', COALESCE(MAX(id), 1) ) FROM public.h_location;
+SELECT SETVAL('public.h_massif_id_seq', COALESCE(MAX(id), 1) ) FROM public.h_massif;
+SELECT SETVAL('public.h_name_id_seq', COALESCE(MAX(id), 1) ) FROM public.h_name;
+SELECT SETVAL('public.h_rigging_id_seq', COALESCE(MAX(id), 1) ) FROM public.h_rigging;
 
 -- t_ tables
-SELECT SETVAL('t_caver_id_seq', (SELECT MAX(id) + 1 FROM public.t_caver));
-SELECT SETVAL('t_bbs_id_seq', (SELECT MAX(id) + 1 FROM public.t_bbs));
-SELECT SETVAL('t_cave_id_seq', (SELECT MAX(id) + 1 FROM public.t_cave));
-SELECT SETVAL('t_caver_id_seq', (SELECT MAX(id) + 1 FROM public.t_caver));
-SELECT SETVAL('t_comment_id_seq', (SELECT MAX(id) + 1 FROM public.t_comment));
-SELECT SETVAL('t_crs_id_seq', (SELECT MAX(id) + 1 FROM public.t_crs));
-SELECT SETVAL('t_description_id_seq', (SELECT MAX(id) + 1 FROM public.t_description));
-SELECT SETVAL('t_document_id_seq', (SELECT MAX(id) + 1 FROM public.t_document));
-SELECT SETVAL('t_entrance_id_seq', (SELECT MAX(id) + 1 FROM public.t_entrance));
-SELECT SETVAL('t_file_id_seq', (SELECT MAX(id) + 1 FROM public.t_file));
-SELECT SETVAL('t_file_format_id_seq', (SELECT MAX(id) + 1 FROM public.t_file_format));
-SELECT SETVAL('t_grotto_id_seq', (SELECT MAX(id) + 1 FROM public.t_grotto));
-SELECT SETVAL('t_history_id_seq', (SELECT MAX(id) + 1 FROM public.t_history));
-SELECT SETVAL('t_location_id_seq', (SELECT MAX(id) + 1 FROM public.t_location));
-SELECT SETVAL('t_massif_id_seq', (SELECT MAX(id) + 1 FROM public.t_massif));
-SELECT SETVAL('t_name_id_seq', (SELECT MAX(id) + 1 FROM public.t_name));
-SELECT SETVAL('t_point_id_seq', (SELECT MAX(id) + 1 FROM public.t_point));
-SELECT SETVAL('t_region_id_seq', (SELECT MAX(id) + 1 FROM public.t_region));
-SELECT SETVAL('t_rigging_id_seq', (SELECT MAX(id) + 1 FROM public.t_rigging));
-SELECT SETVAL('t_right_id_seq', (SELECT MAX(id) + 1 FROM public.t_right));
+SELECT SETVAL('public.t_bbs_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_bbs;
+SELECT SETVAL('public.t_cave_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_cave;
+SELECT SETVAL('public.t_caver_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_caver;
+SELECT SETVAL('public.t_comment_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_comment;
+SELECT SETVAL('public.t_crs_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_crs;
+SELECT SETVAL('public.t_description_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_description;
+SELECT SETVAL('public.t_document_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_document;
+SELECT SETVAL('public.t_entrance_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_entrance;
+SELECT SETVAL('public.t_file_format_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_file_format;
+SELECT SETVAL('public.t_file_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_file;
+SELECT SETVAL('public.t_grotto_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_grotto;
+SELECT SETVAL('public.t_group_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_group;
+SELECT SETVAL('public.t_history_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_history;
+SELECT SETVAL('public.t_location_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_location;
+SELECT SETVAL('public.t_massif_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_massif;
+SELECT SETVAL('public.t_name_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_name;
+SELECT SETVAL('public.t_point_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_point;
+SELECT SETVAL('public.t_region_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_region;
+SELECT SETVAL('public.t_rigging_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_rigging;
+SELECT SETVAL('public.t_right_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_right;
