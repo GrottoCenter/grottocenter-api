efc0f2d61ce0bf05a0df76a651b341358ea4ffb1
Clément Roig <clm.roig@gmail.com>
2021-10-26 16:54:57 +0200

    test(bootstrap): remove auto association id

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -20,13 +20,28 @@ before(function(done) {
       if (err) return done(err);
       // Here you can load fixtures, etc.
       const fixted = new Fixted();
-      fixted.populate(function(err) {
-        if (err) {
-          return done(err);
-        }
-        // Do your thing...
-        done();
-      });
+      fixted.populate(
+        [
+          'tlanguage',
+          'tright',
+          'tidentifiertype',
+          'ttype',
+          'tgroup',
+          'tcaver',
+          'tgrotto',
+          'tname',
+          'tdocument',
+          'tdescription',
+        ],
+        function(err) {
+          if (err) {
+            return done(err);
+          }
+          // Do your thing...
+          done();
+        },
+        false,
+      );
     },
   );
 });
