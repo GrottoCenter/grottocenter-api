a43e47e9329a35c78b21407ec654d21d6369f6bc
Clément Roig <clm.roig@gmail.com>
2021-11-04 14:24:06 +0100

    fix(subject): improve find subject + add test

---
+++ test/integration/2_routes/Subject.test.js
@@ -0,0 +1,33 @@
+let supertest = require('supertest');
+let should = require('should');
+
+describe('Subject features', () => {
+  describe('find()', () => {
+    it('should return code 404', (done) => {
+      supertest(sails.hooks.http.app)
+        .get('/api/v1/documents/subjects/987654321')
+        .set('Content-type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(404, done);
+    });
+    it('should return code 200', (done) => {
+      supertest(sails.hooks.http.app)
+        .get('/api/v1/documents/subjects/4.12')
+        .set('Content-type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          const { body: subject } = res;
+          should(subject).have.properties(['code', 'parent', 'subject']);
+          should(subject.code).equal('4.12');
+          should(subject.subject).equal('AMERICA');
+          should(subject.parent.code).equal('4.1');
+          should(subject.parent.subject).equal(
+            'ARCHAEOLOGY; PREHISTORICAL AND HISTORICAL CULTURES',
+          );
+          return done();
+        });
+    });
+  });
+});
