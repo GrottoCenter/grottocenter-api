e4c5079f7db0b848f6d4910c5fffe71dbec51d18
Clément Roig <clm.roig@gmail.com>
2021-11-08 14:11:13 +0100

    feat(docEndpoint): add swagger yaml endpoint

---
+++ api/controllers/v1/SwaggerController.js
@@ -0,0 +1,19 @@
+/**
+ * SwaggerController.js
+ *
+ * @description :: v1 Swagger controller
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+module.exports = {
+  sendYaml: async (req, res) => {
+    return res.sendFile('swaggerV1.yaml', { root: './assets' }, (err) => {
+      if (err) {
+        sails.log.error(err);
+        return res.serverError(
+          'A server error occured when trying to get the v1 API swagger yaml file.',
+        );
+      }
+    });
+  },
+};
