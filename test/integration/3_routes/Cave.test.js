31527ed9b602a600156dbd718ef2e6f7c607c309
Clément Roig <clm.roig@gmail.com>
2021-11-22 19:15:37 +0100

    test(cave): add findAll cave test and fix method

--- test/integration/3_routes/Cave.test.js
+++ test/integration/3_routes/Cave.test.js
@@ -1,6 +1,26 @@
 let supertest = require('supertest');
 let should = require('should');
 
+const CAVE_PROPERTIES = [
+  '@context',
+  '@id',
+  '@type',
+  'id',
+  'author',
+  'dateInscription',
+  'dateReviewed',
+  'depth',
+  'descriptions',
+  'documents',
+  'entrances',
+  'isDeleted',
+  'isDiving',
+  'length',
+  'name',
+  'names',
+  'temperature',
+];
+
 describe('Cave features', () => {
   describe('find()', () => {
     it('should return code 404', (done) => {
@@ -19,28 +39,32 @@ describe('Cave features', () => {
         .end((err, res) => {
           if (err) return done(err);
           const { body: cave } = res;
-          should(cave).have.properties([
-            '@context',
-            '@id',
-            '@type',
-            'id',
-            'author',
-            'dateInscription',
-            'dateReviewed',
-            'depth',
-            'descriptions',
-            'documents',
-            'entrances',
-            'isDeleted',
-            'isDiving',
-            'length',
-            'name',
-            'names',
-            'temperature',
-          ]);
+          should(cave).have.properties(CAVE_PROPERTIES);
           should(cave.name).not.be.empty();
           should(cave.names).not.be.empty();
           should(cave.author).not.be.empty();
+          return done();
+        });
+    });
+  });
+  describe('findAll()', () => {
+    it('should return code 200', (done) => {
+      supertest(sails.hooks.http.app)
+        .get('/api/v1/caves')
+        .set('Content-type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          const { body: caves } = res;
+          sails.log.info(caves);
+          caves.forEach((cave) => {
+            sails.log.debug(cave);
+            should(cave).have.properties(CAVE_PROPERTIES);
+            should(cave.name).not.be.empty();
+            should(cave.names).not.be.empty();
+            should(cave.author).not.be.empty();
+          });
           return done();
         });
     });
