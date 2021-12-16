40f6f2e70351f0ce5c9dc58359511f5c10d80eca
Clément Roig <clm.roig@gmail.com>
2021-12-16 11:30:17 +0100

    feat(#684): add CC-BY-NC-ND license

--- sql/2_2021_09_22_6_licenses.sql
+++ sql/2_2021_09_22_6_licenses.sql
@@ -4,4 +4,6 @@ VALUES
     (1, 'CC-BY-SA', NULL, true, 'https://creativecommons.org/licenses/by-sa/3.0/'),
     (2, 'CC-BY', NULL, true, 'https://creativecommons.org/licenses/by/3.0/'),
     (3, 'ODBL', NULL, false, 'https://opendatacommons.org/licenses/odbl/'),
-    (4, 'ODC-BY', NULL, false, 'https://opendatacommons.org/licenses/by/');
+    (4, 'ODC-BY', NULL, false, 'https://opendatacommons.org/licenses/by/'),
+    (5, 'Licence Ouverte', NULL, false, 'https://www.etalab.gouv.fr/licence-ouverte-open-licence'),
+    (6, 'CC-BY-NC-ND', NULL, true, 'https://creativecommons.org/licenses/by-nc-nd/2.0/');
