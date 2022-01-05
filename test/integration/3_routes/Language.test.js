81c0722d428c6f52f77867c282641259eb0e2671
Clément Roig <clm.roig@gmail.com>
2022-01-05 12:26:36 +0100

    test(language): add more language tests

---
+++ test/integration/3_routes/Language.test.js
@@ -0,0 +1,60 @@
+let supertest = require('supertest');
+let should = require('should');
+
+const LANGUAGE_PROPERTIES = [
+  'id',
+  'part2b',
+  'part2t',
+  'part1',
+  'scope',
+  'type',
+  'refName',
+  'isPrefered',
+];
+
+describe('Language features', () => {
+  describe('find', () => {
+    it('should return code 404', (done) => {
+      supertest(sails.hooks.http.app)
+        .get('/api/v1/languages/987654321')
+        .set('Content-type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(404, done);
+    });
+    it('should return code 200', (done) => {
+      supertest(sails.hooks.http.app)
+        .get('/api/v1/languages/fra')
+        .set('Content-type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          const { body: language } = res;
+          should(language).have.properties(LANGUAGE_PROPERTIES);
+          should(language.id).not.be.empty();
+          should(language.refName).not.be.empty();
+          return done();
+        });
+    });
+  });
+  describe('findAll', () => {
+    it('should return code 200', (done) => {
+      supertest(sails.hooks.http.app)
+        .get('/api/v1/languages')
+        .set('Content-type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          const { languages } = res.body;
+          should(languages.length).equal(2);
+          for (const language of languages) {
+            should(language).have.properties(LANGUAGE_PROPERTIES);
+            should(language.id).not.be.undefined();
+            should(language.refName).not.be.empty();
+          }
+          return done();
+        });
+    });
+  });
+});
