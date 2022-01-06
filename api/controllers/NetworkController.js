11b57415f8fcd1365347489d7a30d31ba7e77677
Clément Roig <clm.roig@gmail.com>
2022-01-06 17:27:46 +0100

    feat(network): add empty Network files

---
+++ api/controllers/NetworkController.js
@@ -0,0 +1,8 @@
+/**
+ * NetworkController
+ *
+ * @description :: Server-side logic for managing Network. A network is a cave with 2 or more entrances.
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {};
