2804bb5be9708bc6c62b862640bd5120dba003ff
Clément Roig <clm.roig@gmail.com>
2022-01-31 14:53:27 +0100

    fix(test): update test bd sequences before starting

--- test/integration/3_routes/Document.test.js
+++ test/integration/3_routes/Document.test.js
@@ -119,61 +119,59 @@ describe('Document features', () => {
     });
   });
 
-  // TODO : something is going wrong here (409 Conflict HTTP response) maybe because Sails/Waterline doesn't handle auto-incrementing id field correctly.
-  //
-  // describe('Create', () => {
-  //   describe('Minimal Collection data', () => {
-  //     it('should return code 200', (done) => {
-  //       supertest(sails.hooks.http.app)
-  //         .post('/api/v1/documents')
-  //         .send({
-  //           description: 'This is a test collection for test purpose.',
-  //           documentMainLanguage: { id: 'fra' },
-  //           documentType: { id: 1 },
-  //           editor: {
-  //             id: 2,
-  //           },
-  //           isNewDocument: true,
-  //           title: 'Test Collection',
-  //           titleAndDescriptionLanguage: { id: 'fra' },
-  //         })
-  //         .set('Authorization', userToken)
-  //         .set('Content-type', 'application/json')
-  //         .set('Accept', 'application/json')
-  //         .expect(200, done);
-  //     });
-  //   });
-  //   describe('Complete Collection data', () => {
-  //     it('should return code 200', (done) => {
-  //       supertest(sails.hooks.http.app)
-  //         .post('/api/v1/documents')
-  //         .send({
-  //           authors: [{ id: 1 }, { id: 2 }],
-  //           authorComment: 'I am a simple comment',
-  //           description: 'This is a test collection for test purpose.',
-  //           documentMainLanguage: { id: 'fra', refName: 'French' },
-  //           documentType: { id: 1, name: 'Collection' },
-  //           editor: {
-  //             id: 2,
-  //           },
-  //           identifier: 'https://testcol.com',
-  //           identifierType: 'url',
-  //           isNewDocument: true,
-  //           library: null,
-  //           massif: null,
-  //           partOf: null,
-  //           publicationDate: '',
-  //           regions: [],
-  //           startPage: null,
-  //           subjects: [{ code: '1.25' }, { code: '4.0' }],
-  //           title: 'Test Collection',
-  //           titleAndDescriptionLanguage: { id: 'fra', refName: 'French' },
-  //         })
-  //         .set('Authorization', userToken)
-  //         .set('Content-type', 'application/json')
-  //         .set('Accept', 'application/json')
-  //         .expect(200, done);
-  //     });
-  //   });
-  // });
+  describe('Create', () => {
+    describe('Minimal Collection data', () => {
+      it('should return code 200', (done) => {
+        supertest(sails.hooks.http.app)
+          .post('/api/v1/documents')
+          .send({
+            description: 'This is a test collection for test purpose.',
+            documentMainLanguage: { id: 'fra' },
+            documentType: { id: 1 },
+            editor: {
+              id: 2,
+            },
+            isNewDocument: true,
+            title: 'Test Collection',
+            titleAndDescriptionLanguage: { id: 'fra' },
+          })
+          .set('Authorization', userToken)
+          .set('Content-type', 'application/json')
+          .set('Accept', 'application/json')
+          .expect(200, done);
+      });
+    });
+    describe('Complete Collection data', () => {
+      it('should return code 200', (done) => {
+        supertest(sails.hooks.http.app)
+          .post('/api/v1/documents')
+          .send({
+            authors: [{ id: 1 }, { id: 2 }],
+            authorComment: 'I am a simple comment',
+            description: 'This is a test collection for test purpose.',
+            documentMainLanguage: { id: 'fra', refName: 'French' },
+            documentType: { id: 1, name: 'Collection' },
+            editor: {
+              id: 2,
+            },
+            identifier: 'https://testcol.com',
+            identifierType: 'url',
+            isNewDocument: true,
+            library: null,
+            massif: null,
+            partOf: null,
+            publicationDate: '',
+            regions: [],
+            startPage: null,
+            subjects: [{ code: '1.25' }, { code: '4.0' }],
+            title: 'Test Collection',
+            titleAndDescriptionLanguage: { id: 'fra', refName: 'French' },
+          })
+          .set('Authorization', userToken)
+          .set('Content-type', 'application/json')
+          .set('Accept', 'application/json')
+          .expect(200, done);
+      });
+    });
+  });
 });
---
+++ test/update_sequences.js
@@ -0,0 +1,25 @@
+const UPDATE_SEQUENCES_QUERY = `
+-- See this page to know how to get all the sequences updated: https://wiki.postgresql.org/wiki/Fixing_Sequences
+
+-- t_ tables
+SELECT SETVAL('public.t_cave_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_cave;
+SELECT SETVAL('public.t_caver_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_caver;
+SELECT SETVAL('public.t_comment_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_comment;
+SELECT SETVAL('public.t_description_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_description;
+SELECT SETVAL('public.t_document_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_document;
+SELECT SETVAL('public.t_entrance_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_entrance;
+SELECT SETVAL('public.t_file_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_file;
+SELECT SETVAL('public.t_grotto_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_grotto;
+SELECT SETVAL('public.t_group_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_group;
+SELECT SETVAL('public.t_history_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_history;
+SELECT SETVAL('public.t_location_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_location;
+SELECT SETVAL('public.t_massif_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_massif;
+SELECT SETVAL('public.t_name_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_name;
+SELECT SETVAL('public.t_option_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_option;
+SELECT SETVAL('public.t_point_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_point;
+SELECT SETVAL('public.t_region_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_region;
+SELECT SETVAL('public.t_rigging_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_rigging;
+SELECT SETVAL('public.t_right_id_seq', COALESCE(MAX(id), 1) ) FROM public.t_right;
+`;
+
+module.exports = UPDATE_SEQUENCES_QUERY;
