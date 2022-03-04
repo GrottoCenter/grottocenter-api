c348a2feaeb340f78d70fb6850e0f48ab2a3069f
Clément Roig <clm.roig@gmail.com>
2022-03-04 15:20:53 +0100

    improvement(descService): allow setDocDesc to run on null document

--- api/services/DescriptionService.js
+++ api/services/DescriptionService.js
@@ -6,13 +6,15 @@ module.exports = {
    * @param {Boolean} [setParent=true] specify if the document parent description must be set (recursive and cost a lot of resources)
    */
   setDocumentDescriptions: async (document, setParent = true) => {
-    document.descriptions = await TDescription.find()
-      .where({
-        document: document.id,
-      })
-      .populate('language');
-    if (setParent && ramda.pathOr(null, ['parent', 'id'], document)) {
-      await module.exports.setDocumentDescriptions(document.parent);
+    if (document) {
+      document.descriptions = await TDescription.find()
+        .where({
+          document: document.id,
+        })
+        .populate('language');
+      if (setParent && ramda.pathOr(null, ['parent', 'id'], document)) {
+        await module.exports.setDocumentDescriptions(document.parent);
+      }
     }
   },
 };
