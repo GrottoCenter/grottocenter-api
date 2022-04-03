29b429d7339c37d30b5b83f305059b82eeb7f5e7
Clément Roig <clm.roig@gmail.com>
2022-04-03 13:05:07 +0200

    refactor(#741): refactor entranceDuplicateController

---
+++ api/controllers/v1/entrance-duplicate/find-all.js
@@ -0,0 +1,41 @@
+const ControllerService = require('../../../services/ControllerService');
+const ErrorService = require('../../../services/ErrorService');
+const MappingV1Service = require('../../../services/MappingV1Service');
+
+module.exports = async (req, res) => {
+  const sort = `${req.param('sortBy', 'dateInscription')} ${req.param(
+    'orderBy',
+    'ASC'
+  )}`;
+  const limit = req.param('limit', 50);
+  const skip = req.param('skip', 0);
+  try {
+    const duplicates = await TEntranceDuplicate.find()
+      .skip(skip)
+      .limit(limit)
+      .sort(sort)
+      .populate('author');
+    const totalNb = await TEntranceDuplicate.count();
+
+    const params = {
+      controllerMethod: 'EntranceDuplicateController.findAll',
+      limit,
+      searchedItem: 'Entrance duplicates',
+      skip,
+      total: totalNb,
+      url: req.originalUrl,
+    };
+
+    return ControllerService.treatAndConvert(
+      req,
+      null,
+      duplicates,
+      params,
+      res,
+      MappingV1Service.convertToEntranceDuplicateList
+    );
+  } catch (err) {
+    ErrorService.getDefaultErrorHandler(res)(err);
+    return false;
+  }
+};
