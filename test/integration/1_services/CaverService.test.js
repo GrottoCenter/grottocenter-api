b7b04f6a8666afd800be24ada8384e8e8437a2a5
Axel Laget <axel.laget@gmail.com>
2022-04-07 11:00:44 +0200

    fix(#770): prettier format + reformat + id in tcaver

--- test/integration/1_services/CaverService.test.js
+++ test/integration/1_services/CaverService.test.js
@@ -93,57 +93,60 @@ describe('CaverService', () => {
     });
     it('should return a partial view of the caver when providing an user token', async () => {
       const caver = await CaverService.getCaver(6, userReq);
+      should(caver.id).equal(6);
       should(caver.name).equal('Axel');
       should(caver.nickname).equal('Caver1');
       should(caver.surname).equal('Cavo');
       should(caver.documents.length).equal(3);
       should(caver.documents).containDeep([{ id: 1 }, { id: 2 }, { id: 4 }]);
-      should(caver.groups.length).equal(1)
-      should(caver.groups).containDeep([{id: 1}])
-      should(caver.exploredEntrances.length).equal(1)
-      should(caver.exploredEntrances).containDeep([{id: 4}])
+      should(caver.groups.length).equal(1);
+      should(caver.groups).containDeep([{ id: 1 }]);
+      should(caver.exploredEntrances.length).equal(1);
+      should(caver.exploredEntrances).containDeep([{ id: 4 }]);
       caver.exploredEntrances.forEach((entrance) => {
-        should(entrance.isPublic).equal(true)
+        should(entrance.isPublic).equal(true);
       });
-      should(caver.exploredEntrances)
-      should(caver.language).equal('fra')
+      should(caver.exploredEntrances);
+      should(caver.language).equal('fra');
       should.not.exist(caver.password);
       should.not.exist(caver.activationCode);
     });
-    
+
     it('should return a partial view of the caver when not providing a token', async () => {
       const caver = await CaverService.getCaver(6, {});
+      should(caver.id).equal(6);
       should(caver.name).equal('Axel');
       should(caver.nickname).equal('Caver1');
       should(caver.surname).equal('Cavo');
       should(caver.documents.length).equal(3);
       should(caver.documents).containDeep([{ id: 1 }, { id: 2 }, { id: 4 }]);
-      should(caver.groups.length).equal(1)
-      should(caver.groups).containDeep([{id: 1}])
-      should(caver.exploredEntrances.length).equal(1)
-      should(caver.exploredEntrances).containDeep([{id: 4}])
+      should(caver.groups.length).equal(1);
+      should(caver.groups).containDeep([{ id: 1 }]);
+      should(caver.exploredEntrances.length).equal(1);
+      should(caver.exploredEntrances).containDeep([{ id: 4 }]);
       caver.exploredEntrances.forEach((entrance) => {
-        should(entrance.isPublic).equal(true)
+        should(entrance.isPublic).equal(true);
       });
-      should(caver.language).equal('fra')
+      should(caver.language).equal('fra');
       should.not.exist(caver.password);
       should.not.exist(caver.activationCode);
     });
     it('should return a complete view of the caver', async () => {
       const caver = await CaverService.getCaver(6, adminReq);
+      should(caver.id).equal(6);
       should(caver.name).equal('Axel');
       should(caver.nickname).equal('Caver1');
       should(caver.surname).equal('Cavo');
       should(caver.documents.length).equal(3);
       should(caver.documents).containDeep([{ id: 1 }, { id: 2 }, { id: 4 }]);
-      should(caver.groups.length).equal(1)
-      should(caver.groups).containDeep([{id: 1}])
-      should(caver.exploredEntrances.length).equal(1)
-      should(caver.exploredEntrances).containDeep([{id: 4}])
+      should(caver.groups.length).equal(1);
+      should(caver.groups).containDeep([{ id: 1 }]);
+      should(caver.exploredEntrances.length).equal(1);
+      should(caver.exploredEntrances).containDeep([{ id: 4 }]);
       caver.exploredEntrances.forEach((entrance) => {
-        should(entrance.isPublic).equal(true)
+        should(entrance.isPublic).equal(true);
       });
-      should(caver.language).equal('fra')
+      should(caver.language).equal('fra');
       should.not.exist(caver.password);
       should.not.exist(caver.activationCode);
 
@@ -153,6 +156,5 @@ describe('CaverService', () => {
       should.exist(caver.grottos);
       should.exist(caver.groups);
     });
-    
   });
 });
