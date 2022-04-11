91f0df05bfa306334c8f12f6dd2c07b07e478957
Axel Laget <axel.laget@gmail.com>
2022-04-11 18:43:55 +0200

    feat(#785): add update.js

--- api/controllers/v1/caver/update.js
+++ api/controllers/v1/caver/update.js
@@ -1,25 +1,47 @@
 const ControllerService = require('../../../services/ControllerService');
-const CaverService = require('../../../services/CaverService');
 const ErrorService = require('../../../services/ErrorService');
 const MappingV1Service = require('../../../services/MappingV1Service');
 const RightService = require('../../../services/RightService');
 
 // eslint-disable-next-line consistent-return
 module.exports = async (req, res) => {
-  // Check right
-  const hasRight = await sails.helpers.checkRight
+  // list of propreties to update
+  const propretiesUpdatable = [
+    'name',
+    'nickname',
+    'mail',
+    'password',
+    'surname',
+  ];
+  // Check right (Admin can't edit mail and password)
+  const hasRightToEditAll = await sails.helpers.checkRight
     .with({
       groups: req.token.groups,
       rightEntity: RightService.RightEntities.CAVER,
-      rightAction: RightService.RightActions.EDIT_ANY,
+      rightAction: RightService.RightActions.EDIT_OWN,
     })
     .intercept('rightNotFound', () =>
       res.serverError(
         'A server error occured when checking your right to update a caver.'
       )
     );
-  if (!hasRight) {
-    return res.forbidden('You are not authorized to update a caver.');
+
+  const hasRightToEditPartially = await sails.helpers.checkRight
+    .with({
+      groups: req.token.groups,
+      rightEntity: RightService.RightEntities.CAVER,
+      rightAction: RightService.RightActions.EDIT_CAVER_PARTIALLY,
+    })
+    .intercept('rightNotFound', () =>
+      res.serverError(
+        'A server error occured when checking your right to update a caver.'
+      )
+    );
+
+  if (!hasRightToEditPartially) {
+    if (!hasRightToEditAll) {
+      return res.forbidden('You are not authorized to update a caver.');
+    }
   }
 
   // Check if caver exists
@@ -31,25 +53,32 @@ module.exports = async (req, res) => {
       sailsModel: TCaver,
     }))
   ) {
-    return res.badRequest(`Could not find caver with id ${caverId}.`);
+    res.status(404);
+    return res.json({ error: `Could not find caver with id ${caverId}.` });
   }
-  const cleanedData = {
-    ...CaverService.getConvertedDataFromClientRequest(req),
-    // eslint-disable-next-line object-shorthand
-    caverId: caverId,
-  };
 
-  // Delete password and email if admin
+  // Check if the changes requested are authorized (check propretiesUpdatable)
+  const keys = Object.keys(req.body);
+  // eslint-disable-next-line consistent-return
+  keys.forEach((prop) => {
+    if (!propretiesUpdatable.includes(prop)) {
+      return res.badRequest(
+        `Could not update property ${prop}, it is not a property which is updatable.`
+      );
+    }
+    if (prop === 'mail' || prop === 'password') {
+      if (hasRightToEditPartially) {
+        return res.forbidden(`Admin can not update property ${prop}`);
+      }
+    }
+  });
 
   // Launch update request using transaction: it performs a rollback if an error occurs
   try {
     await sails.getDatastore().transaction(async (db) => {
-      const updatedCaver = await TCaver.updateOne({
-        id: caverId,
-      })
-        .set(cleanedData)
+      const updatedCaver = await TCaver.updateOne(caverId)
+        .set(req.body)
         .usingConnection(db);
-
       const params = {};
       params.controllerMethod = 'CaverController.update';
       return ControllerService.treatAndConvert(
