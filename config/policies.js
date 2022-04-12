003732d1ee17c9a64ab66d2adadd8fa10822771e
Axel Laget <axel.laget@gmail.com>
2022-04-12 12:10:37 +0200

    fix(#785): modify policies and add rigjts

--- config/policies.js
+++ config/policies.js
@@ -57,7 +57,7 @@ module.exports.policies = {
   'v1/caver/remove-from-group': ['tokenAuth', 'adminAuth'],
   'v1/caver/set-groups': ['tokenAuth', 'adminAuth'],
   'v1/caver/users-count': true,
-  'v1/caver/update': ['tokenAuth', 'adminAuth'],
+  'v1/caver/update': 'tokenAuth',
   'v1/convert/convert': true,
 
   // Description
--- sql/2_2021_09_22_2_rights.sql
+++ sql/2_2021_09_22_2_rights.sql
@@ -65,3 +65,4 @@ VALUES (1, 'Application - view any', NULL),
     (108, 'Entrance Duplicate - create', NULL),
     (109, 'Document Duplicate - delete any', NULL),
     (110, 'Entrance Duplicate - delete any', NULL);
+    (111, 'Author - edit any', NULL);
