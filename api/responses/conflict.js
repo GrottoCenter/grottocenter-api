c2b28f003d9aa322d5dd0ef697a094f69636a819
Clement <clm.roig@gmail.com>
2022-05-26 10:53:34 +0200

    refactor(#725): refactor responses

--- api/responses/conflict.js
+++ api/responses/conflict.js
@@ -1,26 +1,26 @@
 /**
- * 401 (Unauthorized) Handler
+ * 409 (Conflict) Handler
  *
  * Usage:
- * return res.unauthorized();
- * return res.unauthorized(err);
+ * return res.conflict();
+ * return res.conflict(err);
  *
  * e.g.:
  * ```
- * return res.unauthorized('Unauthorized.');
+ * return res.conflict('A resource already exists with conflicting attribute.');
  * ```
  *
  * @param  {Object | string | undefined} data
  */
 
-module.exports = function unauthorized(data) {
+module.exports = function forbidden(data) {
   const { req } = this;
   const { res } = this;
   const sails = req._sails; // eslint-disable-line no-underscore-dangle
   const logResponse = sails.helpers.logResponse.with;
   const formatResponseData = sails.helpers.formatResponseData.with;
 
-  const httpCode = 401;
+  const httpCode = 409;
   res.status(httpCode);
   logResponse.with({ httpCode, data });
   return res.json(formatResponseData.with({ data }));
--- api/responses/serverError.js
+++ api/responses/serverError.js
@@ -5,90 +5,24 @@
  * Usage:
  * return res.serverError();
  * return res.serverError(err);
- * return res.serverError(err, 'some/specific/error/view');
  *
  * NOTE:
  * If something throws in a policy or controller, or an internal
  * error is encountered, Sails will call `res.serverError()`
  * automatically.
+ *
+ * @param  {Object | string | undefined} data
  */
 
-module.exports = function serverError(data, options) {
-  // Get access to `req`, `res`, & `sails`
+module.exports = function serverError(data) {
   const { req } = this;
   const { res } = this;
   const sails = req._sails; // eslint-disable-line no-underscore-dangle
+  const logResponse = sails.helpers.logResponse.with;
+  const formatResponseData = sails.helpers.formatResponseData.with;
 
-  // Set status code
-  res.status(500);
-
-  // Log error to console
-  if (data !== undefined) {
-    sails.log.error('Sending 500 ("Server Error") response: \n', data);
-  } else sails.log.error('Sending empty 500 ("Server Error") response');
-
-  // Only include errors in response if application environment
-  // is not set to 'production'.  In production, we shouldn't
-  // send back any identifying information about errors.
-  if (sails.config.environment === 'production') {
-    data = undefined;
-  }
-
-  // If the user-agent wants JSON, always respond with JSON
-  if (req.wantsJSON) {
-    // If data is a plain string, cast it to json with a message key
-    if (typeof data === 'string') {
-      return res.json({ message: data });
-    }
-    return res.json(data);
-  }
-
-  // If second argument is a string, we take that to mean it refers to a view.
-  // If it was omitted, use an empty object (`{}`)
-  options =
-    typeof options === 'string'
-      ? {
-          view: options,
-        }
-      : options || {};
-
-  // If a view was provided in options, serve it.
-  // Otherwise try to guess an appropriate view, or if that doesn't
-  // work, just send JSON.
-  if (options.view) {
-    return res.view(options.view, {
-      data,
-    });
-  }
-  // If no second argument provided, try to serve the default view,
-  // but fall back to sending JSON(P) if any errors occur.
-  return res.view(
-    '500',
-    {
-      data,
-    },
-    (err, html) => {
-      // If a view error occured, fall back to JSON(P).
-      if (err) {
-        //
-        // Additionally:
-        // • If the view was missing, ignore the error but provide a verbose log.
-        if (err.code === 'E_VIEW_FAILED') {
-          sails.log.verbose(
-            'res.serverError() :: Could not locate view for error page (sending JSON instead).  Details: ',
-            err
-          );
-        } else {
-          // Otherwise, if this was a more serious error, log to the console with the details.
-          sails.log.warn(
-            'res.serverError() :: When attempting to render error page view, an error occured (sending JSON instead).  Details: ',
-            err
-          );
-        }
-        return res.json(data);
-      }
-
-      return res.send(html);
-    }
-  );
+  const httpCode = 500;
+  res.status(httpCode);
+  logResponse.with({ httpCode, data });
+  return res.json(formatResponseData.with({ data }));
 };
