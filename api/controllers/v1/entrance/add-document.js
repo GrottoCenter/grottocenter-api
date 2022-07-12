a489b9ab4fae23c66ab76d54ca57a691f82fdd53
Clement <clm.roig@gmail.com>
2022-07-12 17:42:44 +0200

    fix(entranceAddDoc): fix update method to make it work with histo

--- api/controllers/v1/entrance/add-document.js
+++ api/controllers/v1/entrance/add-document.js
@@ -39,9 +39,15 @@ module.exports = async (req, res) => {
     return res.notFound({ message: `Document of id ${documentId} not found.` });
   }
 
-  // Update entrance
   try {
-    await TEntrance.addToCollection(entranceId, 'documents', documentId);
+    /** 
+     * TEntrance.addToCollection() is not used here because of the Grottocenter's historization process.
+     * To avoid an uniqueness error, the dateReviewed must be set at the same time the entrance id is set, which can't be accomplished with addToCollection().
+     * */ 
+    await TDocument.updateOne(documentId).set({
+      dateReviewed: new Date(),
+      entrance: entranceId,
+    })
     return res.ok();
   } catch (e) {
     return ErrorService.getDefaultErrorHandler(res)(e);
