e886b8df5f522decfacd73e63831cf26ab8718b8
Clément Roig <clm.roig@gmail.com>
2022-09-01 16:04:06 +0200

    fix(docFile): improve error management of doc files update & creation

--- api/controllers/v1/document/update.js
+++ api/controllers/v1/document/update.js
@@ -4,6 +4,9 @@ const DescriptionService = require('../../../services/DescriptionService');
 const DocumentService = require('../../../services/DocumentService');
 const ErrorService = require('../../../services/ErrorService');
 const FileService = require('../../../services/FileService');
+
+const { INVALID_FORMAT, INVALID_NAME, ERROR_DURING_UPLOAD_TO_AZURE } =
+  FileService;
 const MappingService = require('../../../services/MappingService');
 const RightService = require('../../../services/RightService');
 
@@ -35,18 +38,34 @@ module.exports = async (req, res) => {
   const newFilesArray = [];
   if (req.files && req.files.files) {
     const { files } = req.files;
-    for (const file of files) {
-      try {
-        // eslint-disable-next-line no-await-in-loop
-        const createdFile = await FileService.create(
-          file,
-          req.param('id'),
-          true,
-          false
-        );
-        newFilesArray.push(createdFile);
-      } catch (err) {
-        return res.serverError(err);
+    try {
+      await Promise.all(
+        files.map(async (file) => {
+          const createdFile = await FileService.create(
+            file,
+            req.param('id'),
+            true,
+            false
+          );
+          newFilesArray.push(createdFile);
+        })
+      );
+    } catch (err) {
+      const { message, fileName } = err;
+      switch (message) {
+        case INVALID_FORMAT:
+          return res.badRequest(
+            `The format of the file "${fileName}" is invalid.`
+          );
+        case INVALID_NAME:
+          return res.badRequest(
+            `The name of the file "${fileName}" is invalid.`
+          );
+        case ERROR_DURING_UPLOAD_TO_AZURE:
+        default:
+          return res.serverError(
+            'An error occured when trying to upload the file to Azure.'
+          );
       }
     }
   }
