bc1be26ee570396d330fe6ccee41566a35f09307
Clément Roig <clm.roig@gmail.com>
2022-09-07 12:17:28 +0200

    feat(#880): ubsubscribe from a massif & a country

--- test/integration/4_routes/Countries/subscribe.test.js
+++ test/integration/4_routes/Countries/subscribe.test.js
@@ -4,6 +4,7 @@ const AuthTokenService = require('../../AuthTokenService');
 
 describe('Country features', () => {
   let leaderToken;
+  const countryId = 'FR';
   before(async () => {
     leaderToken = await AuthTokenService.getRawBearerLeaderToken();
   });
@@ -18,7 +19,6 @@ describe('Country features', () => {
     });
 
     it('should return 204 on country subscription', (done) => {
-      const countryId = 'FR';
       supertest(sails.hooks.http.app)
         .post(`/api/v1/countries/${countryId}/subscribe`)
         .set('Authorization', leaderToken)
@@ -35,6 +35,43 @@ describe('Country features', () => {
           should(updatedCaver.subscribedToCountries).containDeep([
             { id: countryId },
           ]);
+          return done();
+        });
+    });
+
+    it('should return 404 on trying to unsubscribe from an inexsisting country', (done) => {
+      supertest(sails.hooks.http.app)
+        .post('/api/v1/countries/123456789/unsubscribe')
+        .set('Authorization', leaderToken)
+        .set('Content-type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(404, done);
+    });
+
+    it('should return 422 on trying to unsubscribe from an inexisting subscription', (done) => {
+      supertest(sails.hooks.http.app)
+        .post('/api/v1/countries/GB/unsubscribe')
+        .set('Authorization', leaderToken)
+        .set('Content-type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(422, done);
+    });
+
+    it('should return 204 on country unsubscription', (done) => {
+      supertest(sails.hooks.http.app)
+        .post(`/api/v1/countries/${countryId}/unsubscribe`)
+        .set('Authorization', leaderToken)
+        .set('Content-type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(204)
+        .end(async (err) => {
+          if (err) return done(err);
+
+          // Leader has id = 7
+          const updatedCaver = await TCaver.findOne(7).populate(
+            'subscribedToCountries'
+          );
+          should(updatedCaver.subscribedToCountries).have.length(0);
           return done();
         });
     });
