1872fdba862866b74baced8a72acf15f2d7ad038
Clément Roig <clm.roig@gmail.com>
2022-09-26 16:27:52 +0200

    feat(#1015): add notifications creation for riggings

--- api/controllers/v1/rigging/update.js
+++ api/controllers/v1/rigging/update.js
@@ -1,6 +1,11 @@
 const ControllerService = require('../../../services/ControllerService');
 const ErrorService = require('../../../services/ErrorService');
 const MappingService = require('../../../services/MappingService');
+const {
+  NOTIFICATION_TYPES,
+  NOTIFICATION_ENTITIES,
+} = require('../../../services/NotificationService');
+const NotificationService = require('../../../services/NotificationService');
 const RightService = require('../../../services/RightService');
 
 module.exports = async (req, res) => {
@@ -58,6 +63,14 @@ module.exports = async (req, res) => {
       .populate('cave')
       .populate('language')
       .populate('reviewer');
+
+    await NotificationService.notifySubscribers(
+      req,
+      populatedRigging,
+      req.token.id,
+      NOTIFICATION_TYPES.UPDATE,
+      NOTIFICATION_ENTITIES.RIGGING
+    );
 
     const params = {};
     params.controllerMethod = 'RiggingController.update';
