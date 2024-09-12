65249257cda7239d4ac6ef02d569bf22d25b08d4
Vincent Marseguerra <v.marseguerra@gmail.com>
2024-09-12 09:29:39 +0200

    refactor(global): removes unused dependencies

--- api/controllers/v1/entrance-duplicate/create-from-duplicate.js
+++ api/controllers/v1/entrance-duplicate/create-from-duplicate.js
@@ -1,5 +1,4 @@
 const EntranceService = require('../../../services/EntranceService');
-const ErrorService = require('../../../services/ErrorService');
 const RightService = require('../../../services/RightService');
 
 module.exports = async (req, res) => {
@@ -12,28 +11,17 @@ module.exports = async (req, res) => {
       'You are not authorized to create an entrance duplicate.'
     );
   }
-  if (
-    !(await sails.helpers.checkIfExists.with({
-      attributeName: 'id',
-      attributeValue: req.param('id'),
-      sailsModel: TEntranceDuplicate,
-    }))
-  ) {
-    return res.badRequest(
-      `Could not find duplicate with id ${req.param('id')}.`
-    );
-  }
 
   const id = req.param('id');
-
-  const duplicate = await TEntranceDuplicate.findOne(id);
-
-  const { entrance, nameDescLoc } = duplicate.content;
-  try {
-    await EntranceService.createEntrance(req, entrance, nameDescLoc);
-    await TEntranceDuplicate.destroyOne(id);
-    return res.ok();
-  } catch (e) {
-    return ErrorService.getDefaultErrorHandler(res)(e);
+  const entranceDuplicate = await TEntranceDuplicate.findOne({ id });
+  if (!entranceDuplicate) {
+    return res.badRequest(`Could not find duplicate with id ${id}.`);
   }
+
+  const { entrance, nameDescLoc } = entranceDuplicate.content;
+
+  await EntranceService.createEntrance(req, entrance, nameDescLoc);
+  await TEntranceDuplicate.destroyOne(id);
+
+  return res.ok();
 };
