611141f7fde9ffecf107d4c0e584063a54e39fe6
Vincent Marseguerra <v.marseguerra@gmail.com>
2024-10-23 14:12:11 +0200

    fix(home): increase carousel page size to 50 #1258

--- api/controllers/v1/partner/find-for-carousel.js
+++ api/controllers/v1/partner/find-for-carousel.js
@@ -2,42 +2,38 @@ const ControllerService = require('../../../services/ControllerService');
 const { toOrganization } = require('../../../services/mapping/converters');
 const { toListFromController } = require('../../../services/mapping/utils');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   let skip = 0;
+  let limit = 50;
   if (req.param('skip')) {
     skip = req.param('skip');
   }
-  let limit = 30;
   if (req.param('limit')) {
     limit = req.param('limit');
   }
-  TGrotto.find({ select: ['id', 'pictureFileName', 'customMessage'] })
+  const found = await TGrotto.find({
+    select: ['id', 'pictureFileName', 'customMessage'],
+  })
+    .where({
+      customMessage: { '!=': null },
+      pictureFileName: { '!=': '' },
+      isOfficialPartner: '1',
+    })
     .skip(skip)
     .limit(limit)
     .sort('id ASC')
-    .where({
-      customMessage: {
-        '!=': null,
-      },
-      pictureFileName: {
-        '!=': '',
-      },
-      isOfficialPartner: '1',
-    })
-    .sort('id ASC')
-    .populate('names')
-    .exec((err, found) => {
-      const params = {};
-      params.controllerMethod = 'PartnerController.findForCarousel';
-      params.notFoundMessage = 'No partners found.';
-      return ControllerService.treatAndConvert(
-        req,
-        err,
-        found,
-        params,
-        res,
-        (data, meta) =>
-          toListFromController('organization', data, toOrganization, { meta })
-      );
-    });
+    .populate('names');
+
+  return ControllerService.treatAndConvert(
+    req,
+    null,
+    found,
+    {
+      controllerMethod: 'PartnerController.findForCarousel',
+      notFoundMessage: 'No partners found.',
+    },
+    res,
+    (data, meta) =>
+      toListFromController('organization', data, toOrganization, { meta })
+  );
 };
