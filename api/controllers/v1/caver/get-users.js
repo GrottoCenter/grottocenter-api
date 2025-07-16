54e66af2dccb3bd98fdaac7c477dcc96ac9c7d7d
YohannSchatt <yohann.schatt@gmail.com>
2025-07-16 14:37:08 +0000

    feat(api): add routes to get users, contributors, authors

---
+++ api/controllers/v1/caver/get-users.js
@@ -0,0 +1,32 @@
+const ControllerService = require('../../../services/ControllerService');
+const RightService = require('../../../services/RightService');
+
+module.exports = async (req, res) => {
+  const hasAdmin = RightService.hasGroup(
+    req.token.groups,
+    RightService.G.ADMINISTRATOR
+  );
+
+  const hasModerator = RightService.hasGroup(
+    req.token.groups,
+    RightService.G.MODERATOR
+  );
+
+  if (!hasAdmin && !hasModerator) {
+    return res.forbidden('You are not authorized to access this endpoint');
+  }
+
+  const cavers = await sails.models.vcaverroles.find({ isUser: true });
+
+  if (!cavers) {
+    return res.notFound({ message: 'No users found.' });
+  }
+
+  return ControllerService.treat(
+    req,
+    null,
+    cavers,
+    { controllerMethod: 'CaverController.getUsers' },
+    res
+  );
+};
