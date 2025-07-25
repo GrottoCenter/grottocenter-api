e4180063d7af21b7a8bdca05b27311c9ec7815fb
YohannSchatt <yohann.schatt@gmail.com>
2025-07-25 16:39:08 +0000

    feat(controller): add page for GET authors, users and contributors

--- api/controllers/v1/caver/get-contributors.js
+++ api/controllers/v1/caver/get-contributors.js
@@ -16,11 +16,40 @@ module.exports = async (req, res) => {
     return res.forbidden('You are not authorized to access this endpoint');
   }
 
-  const cavers = await sails.models.vcaverroles.find({ isContributor: true });
+  const page = parseInt(req.params.page, 10);
+  if (Number.isNaN(page) || page <= 0) {
+    return res.badRequest({
+      message: 'Invalid page parameter. Page must be a positive integer.',
+    });
+  }
 
-  if (!cavers) {
+  const limit = 100;
+  const skip = (page - 1) * limit;
+  const total = await sails.models.vcaverroles.count({ isContributor: true });
+  const totalPages = Math.ceil(total / limit);
+  if (page > totalPages) {
+    return res.notFound({ message: 'Page not found' });
+  }
+
+  const caversPage = await sails.models.vcaverroles
+    .find({ isContributor: true })
+    .limit(limit)
+    .skip(skip)
+    .sort('id ASC');
+
+  if (!caversPage.length) {
     return res.notFound({ message: 'No contributors found.' });
   }
+
+  const cavers = {
+    data: caversPage,
+    pagination: {
+      page,
+      limit,
+      total,
+      totalPages,
+    },
+  };
 
   return ControllerService.treat(
     req,
--- test/integration/4_routes/Cavers/getUsers.test.js
+++ test/integration/4_routes/Cavers/getUsers.test.js
@@ -13,37 +13,75 @@ describe('Caver features', () => {
     moderatorToken = await AuthTokenService.getRawBearerModeratorToken();
   });
 
-  it('should return all users with adminToken', (done) => {
+  it('should return page 1 users with adminToken', (done) => {
     supertest(sails.hooks.http.app)
-      .get('/api/v1/cavers/users')
+      .get('/api/v1/cavers/users/1')
       .set('Authorization', adminToken)
       .set('Content-type', 'application/json')
       .set('Accept', 'application/json')
       .expect(200)
       .end((err, res) => {
         if (err) return done(err);
-        res.body.should.be.an.Array().and.have.lengthOf(7);
+        res.body.should.have.property('data');
+        res.body.should.have.property('pagination');
+
+        res.body.data.should.be.an.Array().and.have.lengthOf(100);
+
+        res.body.pagination.should.have.property('page', 1);
+        res.body.pagination.should.have.property('limit', 100);
+        res.body.pagination.should.have.property('total');
+        res.body.pagination.should.have.property('totalPages');
         return done();
       });
   });
 
-  it('should return all users with moderatorToken', (done) => {
+  it('should return page 2 users with adminToken', (done) => {
     supertest(sails.hooks.http.app)
-      .get('/api/v1/cavers/users')
+      .get('/api/v1/cavers/users/2')
+      .set('Authorization', adminToken)
+      .set('Content-type', 'application/json')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.body.should.have.property('data');
+        res.body.should.have.property('pagination');
+
+        res.body.data.should.be.an.Array().and.have.lengthOf(8);
+
+        res.body.pagination.should.have.property('page', 2);
+        res.body.pagination.should.have.property('limit', 100);
+        res.body.pagination.should.have.property('total');
+        res.body.pagination.should.have.property('totalPages');
+        return done();
+      });
+  });
+
+  it('should return page 1 users with moderatorToken', (done) => {
+    supertest(sails.hooks.http.app)
+      .get('/api/v1/cavers/users/1')
       .set('Authorization', moderatorToken)
       .set('Content-type', 'application/json')
       .set('Accept', 'application/json')
       .expect(200)
       .end((err, res) => {
         if (err) return done(err);
-        res.body.should.be.an.Array().and.have.lengthOf(7);
+        res.body.should.have.property('data');
+        res.body.should.have.property('pagination');
+
+        res.body.data.should.be.an.Array().and.have.lengthOf(100);
+
+        res.body.pagination.should.have.property('page', 1);
+        res.body.pagination.should.have.property('limit', 100);
+        res.body.pagination.should.have.property('total');
+        res.body.pagination.should.have.property('totalPages');
         return done();
       });
   });
 
-  it('should return all users with userToken', (done) => {
+  it('should return unauthorized error', (done) => {
     supertest(sails.hooks.http.app)
-      .get('/api/v1/cavers/users')
+      .get('/api/v1/cavers/users/1')
       .set('Authorization', userToken)
       .set('Content-type', 'application/json')
       .set('Accept', 'application/json')
@@ -52,6 +90,38 @@ describe('Caver features', () => {
         if (err) return done(err);
         res.body.should.deepEqual({
           message: 'You are not authorized to access this endpoint',
+        });
+        return done();
+      });
+  });
+
+  it('should return badRequest error', (done) => {
+    supertest(sails.hooks.http.app)
+      .get('/api/v1/cavers/users/-1')
+      .set('Authorization', adminToken)
+      .set('Content-type', 'application/json')
+      .set('Accept', 'application/json')
+      .expect(400)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.body.should.deepEqual({
+          message: 'Invalid page parameter. Page must be a positive integer.',
+        });
+        return done();
+      });
+  });
+
+  it('should return 404 error', (done) => {
+    supertest(sails.hooks.http.app)
+      .get('/api/v1/cavers/users/100')
+      .set('Authorization', adminToken)
+      .set('Content-type', 'application/json')
+      .set('Accept', 'application/json')
+      .expect(404)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.body.should.deepEqual({
+          message: 'Page not found',
         });
         return done();
       });
