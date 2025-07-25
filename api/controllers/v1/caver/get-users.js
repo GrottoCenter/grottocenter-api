1e7f5874131460cdab7dbdd836bb1d6911d82994
YohannSchatt <yohann.schatt@gmail.com>
2025-07-25 16:39:08 +0000

    feat(controller): add page for GET authors, users and contributors

--- api/controllers/v1/caver/get-users.js
+++ api/controllers/v1/caver/get-users.js
@@ -16,11 +16,40 @@ module.exports = async (req, res) => {
     return res.forbidden('You are not authorized to access this endpoint');
   }
 
-  const cavers = await sails.models.vcaverroles.find({ isUser: true });
+  const page = parseInt(req.params.page, 10);
+  if (Number.isNaN(page) || page <= 0) {
+    return res.badRequest({
+      message: 'Invalid page parameter. Page must be a positive integer.',
+    });
+  }
 
-  if (!cavers) {
+  const limit = 100;
+  const skip = (page - 1) * limit;
+  const total = await sails.models.vcaverroles.count({ isUser: true });
+  const totalPages = Math.ceil(total / limit);
+  if (page > totalPages) {
+    return res.notFound({ message: 'Page not found' });
+  }
+
+  const caversPage = await sails.models.vcaverroles
+    .find({ isUser: true })
+    .limit(limit)
+    .skip(skip)
+    .sort('id ASC');
+
+  if (!caversPage.length) {
     return res.notFound({ message: 'No users found.' });
   }
+
+  const cavers = {
+    data: caversPage,
+    pagination: {
+      page,
+      limit,
+      total,
+      totalPages,
+    },
+  };
 
   return ControllerService.treat(
     req,
