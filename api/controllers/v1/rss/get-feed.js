45796ed636da5a15fb08091b1e18603f7cf885b7
Clément Ronzon <clem.rz@gmail.com>
2025-11-18 16:04:56 -0600

    chore(deps): replaces feed-read (security issue) with rss-parser

--- api/controllers/v1/rss/get-feed.js
+++ api/controllers/v1/rss/get-feed.js
@@ -1,4 +1,4 @@
-const feedRead = require('feed-read');
+const Parser = require('rss-parser');
 const underscoreDate = require('underscore.date');
 
 const FR_RSS_FEED =
@@ -8,24 +8,31 @@ const EN_RSS_FEED =
 
 const CommonService = require('../../../services/CommonService');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   // TODO error when language is neither FR nor EN
   const rssFeed = req.params.language === 'FR' ? FR_RSS_FEED : EN_RSS_FEED;
-  feedRead(rssFeed, (err, articles) => {
-    if (err) {
-      return res.serverError({
-        error: err,
-        message: 'An error occurred when getting the RSS feed',
-      });
-    }
+  const parser = new Parser();
+
+  try {
+    const feed = await parser.parseURL(rssFeed);
+    const article = feed.items[0];
 
     const result = {};
-    result.title = CommonService.convertHtmlToText(articles[0].title, 50);
-    result.text = CommonService.convertHtmlToText(articles[0].content, 255);
-    result.link = articles[0].link;
-    result.day = articles[0].published.getDate();
-    result.month = underscoreDate.monthsShort[articles[0].published.getMonth()];
+    result.title = CommonService.convertHtmlToText(article.title, 50);
+    result.text = CommonService.convertHtmlToText(
+      article.content || article.contentSnippet,
+      255
+    );
+    result.link = article.link;
+    result.day = new Date(article.pubDate).getDate();
+    result.month =
+      underscoreDate.monthsShort[new Date(article.pubDate).getMonth()];
 
     return res.json(result);
-  });
+  } catch (err) {
+    return res.serverError({
+      error: err,
+      message: 'An error occurred when getting the RSS feed',
+    });
+  }
 };
