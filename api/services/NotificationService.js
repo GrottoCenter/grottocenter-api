1a5c2ca6c72f1568e118702707f572b0e2ac4cb6
dawoldo <mz.moudni@gmail.com>
2026-05-18 11:00:37 +0200

    feat(1451): update email notification link and remove duplicate translation keys

--- api/services/NotificationService.js
+++ api/services/NotificationService.js
@@ -214,8 +214,7 @@ module.exports = {
         result.rows.map(async (row) => {
           const recipient = await TCaver.findOne({ id: row.id_caver });
           if (!recipient || !recipient.sendMessageNotificationByEmail) return;
-          // TODO: construct conversation link when the  frontend is worked on
-          const conversationLink = '';
+          const conversationLink = `${sails.config.custom.frontendUrl}/ui/messages/${conversationId}`;
           await sails.helpers.sendEmail
             .with({
               allowResponse: false,
