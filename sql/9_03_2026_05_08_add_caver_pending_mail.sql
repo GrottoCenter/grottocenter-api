53e4b2edd5a505bf6eb0e74542431b61d7d8d540
dawoldo <mz.moudni@gmail.com>
2026-05-22 12:08:25 +0200

    feat(1449): add prefix to pending_mail column, database index

--- sql/9_03_2026_05_08_add_caver_pending_mail.sql
+++ sql/9_03_2026_05_08_add_caver_pending_mail.sql
@@ -1,1 +1,4 @@
-ALTER TABLE t_caver ADD COLUMN pending_mail VARCHAR(50) DEFAULT NULL;
+\c grottoce;
+
+ALTER TABLE t_caver ADD COLUMN IF NOT EXISTS pending_mail VARCHAR(50) DEFAULT NULL;
+CREATE INDEX IF NOT EXISTS t_caver_pending_mail_idx ON t_caver (pending_mail);
